a47c44dd9508a2d61c3ec4017addde674eacf655
Jared Parsons <jaredpparsons@gmail.com>
2009-12-01 17:17:54 -0800

    More Command mode fixup

--- VimCore/CM_CommandMode.fs
+++ VimCore/CM_CommandMode.fs
@@ -7,25 +7,18 @@ open System.Windows.Input
 open System.Text.RegularExpressions
 open VimCore.RegexUtil
 
-type CommandModeData = {
-    VimBufferData : IVimBufferData;
-    RunFunc : CommandModeData -> KeyInput -> (CommandModeData * option<ModeKind>)
-}
-    
-module CommandModeUtil = 
-    
-    // Actually process the completed command
-    let ProcessCommand (cmd:string) (d:IVimBufferData) = 
+type CommandMode( _data : IVimBufferData ) = 
+    let mutable _command : System.String = System.String.Empty
+
+        // Actually process the completed command
+    member x.ProcessCommand (cmd:string) (d:IVimBufferData) = 
         let host = d.VimHost
         match cmd with
             | Match2 "^e\s(.*)$" (_,file) -> Util.EditFile host file
             | Match2 "^(\d+)$" (_,lineNum) -> Util.JumpToLineNumber d lineNum
             | Match1 "^\$$" _ -> Util.JumpToLastLine d
             | _ -> host.UpdateStatus("Cannot run \"" + cmd)
-    
-                
-  type CommandMode( _data : IVimBufferData ) = 
-    let mutable _command : System.String = System.String.Empty
+
     interface IMode with 
         member x.Commands = Seq.empty
         member x.ModeKind = ModeKind.Command
@@ -33,7 +26,7 @@ module CommandModeUtil =
         member x.Process ki = 
             match ki.Key with 
                 | Key.Enter ->
-                    CommandModeUtil.ProcessCommand _command _data
+                    x.ProcessCommand _command _data
                     SwitchMode ModeKind.Normal
                 | Key.Escape ->
                     SwitchMode ModeKind.Normal
