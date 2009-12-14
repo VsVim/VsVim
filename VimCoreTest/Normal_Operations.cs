afaf7df8710df105a38da7d6f6857a3fb08186fe
Jared Parsons <jaredpparsons@gmail.com>
2009-12-13 20:57:01 -0800

    Added tests for gp

---
+++ VimCoreTest/Normal_Operations.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+
+namespace VimCoreTest
+{
+    [TestFixture]
+    public class Normal_Operations
+    {
+
+    }
+}
