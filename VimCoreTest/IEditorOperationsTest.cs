bada8a099dd8da515166ffc2fcbe2c6a5b66ad56
Jared Parsons <jaredpparsons@gmail.com>
2009-12-28 16:08:22 -0800

    Added tests to verify assumed behavior about the IEditOperations implementation

--- VimCoreTest/IEditorOperationsTest.cs
+++ VimCoreTest/IEditorOperationsTest.cs
@@ -2,51 +2,47 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using NUnit.Framework;
+using Microsoft.VisualStudio.Text;
+using Microsoft.VisualStudio.Text.Operations;
+using VimCoreTest.Utils;
+using Microsoft.VisualStudio.Text.Editor;
 
 namespace VimCoreTest
 {
-    class IEditorOperationsTest
+    [TestFixture]
+    public class IEditorOperationsTest
     {
-        // TODO:
-        // MoveLineDown 
-        //   - Doesn't crash second to last line
-        //   - Maintains column
+        private ITextView _view;
+        private ITextBuffer _buffer;
+        private IEditorOperations _operations;
 
-        //[Test, Description("Be wary the 0 length last line")]
-        //public void MoveCaretDown5()
-        //{
-        //    CreateLines("foo", String.Empty);
-        //    var tss = _view.TextSnapshot;
-        //    var line = tss.GetLineFromLineNumber(0);
-        //    _view.Caret.MoveTo(line.Start);
-        //    _editorOpts.Setup(x => x.ResetSelection()).Verifiable();
-        //    _editorOpts.Setup(x => x.MoveLineDown(false)).Verifiable();
-        //    _operations.MoveCaretDown(1);
-        //    _editorOpts.Verify();
-        //}
+        public void CreateLines(params string[] lines)
+        {
+            var tuple = EditorUtil.CreateViewAndOperations(lines);
+            _view = tuple.Item1;
+            _buffer = _view.TextBuffer;
+            _operations = tuple.Item2;
+        }
 
-        //[Test, Description("Move caret down should maintain column")]
-        //public void MoveCaretDown6()
-        //{
-        //    CreateLines("foo", "bar");
-        //    var tss = _view.TextSnapshot;
-        //    _view.Caret.MoveTo(tss.GetLineFromLineNumber(0).Start.Add(1));
-        //    _editorOpts.Setup(x => x.ResetSelection()).Verifiable();
-        //    _operations.MoveCaretDown(1);
-        //    Assert.AreEqual(tss.GetLineFromLineNumber(1).Start.Add(1), _view.Caret.Position.BufferPosition);
-        //    _editorOpts.Verify();
-        //}
+        [Test, Description("Be wary the 0 length last line")]
+        public void MoveCaretDown1()
+        {
+            CreateLines("foo", String.Empty);
+            var tss = _view.TextSnapshot;
+            var line = tss.GetLineFromLineNumber(0);
+            _view.Caret.MoveTo(line.Start);
+            _operations.MoveLineDown(false);
+        }
 
-        //[Test, Description("Move caret down should maintain column")]
-        //public void MoveCaretDown7()
-        //{
-        //    CreateLines("foo", "bar");
-        //    var tss = _view.TextSnapshot;
-        //    _view.Caret.MoveTo(tss.GetLineFromLineNumber(0).End);
-        //    _editorOpts.Setup(x => x.ResetSelection()).Verifiable();
-        //    _operations.MoveCaretDown(1);
-        //    Assert.AreEqual(tss.GetLineFromLineNumber(1).End, _view.Caret.Position.BufferPosition);
-        //    _editorOpts.Verify();
-        //}
+        [Test, Description("Move caret down should maintain column")]
+        public void MoveCaretDown2()
+        {
+            CreateLines("foo", "bar");
+            var tss = _view.TextSnapshot;
+            _view.Caret.MoveTo(tss.GetLineFromLineNumber(0).Start.Add(1));
+            _operations.MoveLineDown(false);
+            Assert.AreEqual(tss.GetLineFromLineNumber(1).Start.Add(1), _view.Caret.Position.BufferPosition);
+        }
     }
 }
