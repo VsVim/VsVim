f611ca7a71d0ceeb1d1609664905664b57a67bcb
Jared Parsons <jaredpparsons@gmail.com>
2010-01-18 16:19:36 -0800

    Added a script to populate the references folder.  Enables easier bulid scenarios for different branches

---
+++ Populate-References.ps1
@@ -0,0 +1,41 @@
+$script:scriptPath = split-path -parent $MyInvocation.MyCommand.Definition 
+$refPath = join-path $scriptPath "References"
+$coreDllList = @(   "Microsoft.VisualStudio.CoreUtility.dll",
+                "Microsoft.VisualStudio.Editor.dll",
+                "Microsoft.VisualStudio.Editor.Implementation.dll",
+                "Microsoft.VisualStudio.Language.Intellisense.dll",
+                "Microsoft.VisualStudio.Language.StandardClassification.dll",
+                "Microsoft.VisualStudio.Platform.VSEditor.dll",
+                "Microsoft.VisualStudio.Platform.VSEditor.Interop.dll",
+                "Microsoft.VisualStudio.Text.Data.dll",
+                "Microsoft.VisualStudio.Text.Logic.dll",
+                "Microsoft.VisualStudio.Text.UI.dll",
+                "Microsoft.VisualStudio.Text.UI.Wpf.dll",
+                "Microsoft.VisualStudio.UI.Undo.dll" )
+
+function Get-ProgramFiles32() {
+    if ( test-path (join-path $env:WinDir "SysWow64") ) {
+        return ${env:ProgramFiles(x86)}
+    }
+    
+    return $env:ProgramFiles
+}
+
+function CopyTo-References() {
+    param ( [string]$source = $(throw "Need a source") )
+    if ( -not (test-path $source)) {
+        write-error "Not found $source"
+    }
+    copy $source $refPath
+}
+
+$progPath = Get-ProgramFiles32
+$vsPath = join-path $progPath "Microsoft Visual Studio 10.0\Common7\ide\CommonExtensions\microsoft\Editor"
+foreach ( $dll in $coreDllList) {
+    $fullPath = join-Path $vsPath $dll
+    CopyTo-References $fullPath
+}
+
+$fullPath = join-path $progPath "Microsoft Visual Studio 10.0\Common7\IDE\PrivateAssemblies\Microsoft.VisualStudio.Text.Internal.dll"
+CopyTo-References $fullPath
+
