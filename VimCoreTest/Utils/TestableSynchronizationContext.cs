a509857ab56fa335a5cfec9db32671a0c50a0e12
Jared Parsons <jaredpparsons@gmail.com>
2010-03-11 22:43:35 -0800

    Missing file

---
+++ VimCoreTest/Utils/TestableSynchronizationContext.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace VimCoreTest.Utils
+{
+    internal sealed class TestableSynchronizationContext : SynchronizationContext
+    {
+        private List<Action> _list = new List<Action>();
+
+        public override void Post(SendOrPostCallback d, object state)
+        {
+            _list.Add(() => d(state));
+        }
+
+        public void RunAll()
+        {
+            foreach (var cur in _list)
+            {
+                cur();
+            }
+            _list.Clear();
+        }
+    }
+}
