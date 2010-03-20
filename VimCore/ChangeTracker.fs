caa6ecffafdf11335a05bdcc30ecc1b8351da35b
Jared Parsons <jaredpparsons@gmail.com>
2010-03-20 02:50:23 -0700

    Most of repeat implemented

---
+++ VimCore/ChangeTracker.fs
@@ -0,0 +1,25 @@
+﻿#light
+
+namespace Vim
+
+open Microsoft.VisualStudio.Text
+open Microsoft.VisualStudio.Text.Editor
+open Microsoft.VisualStudio.Text.Operations
+
+type internal ChangeTracker() =
+
+    let mutable _last : RepeatableChange option = None
+    
+    member private x.OnVimBufferCreated (buffer:IVimBuffer) =
+        buffer.NormalMode.CommandExecuted |> Event.add x.OnCommandExecuted
+
+    member private x.OnCommandExecuted command = 
+        match command with
+        | NonRepeatableCommand -> _last <- None
+        | RepeatableCommand(keyInputs,count,reg) -> _last <- NormalModeChange(keyInputs,count,reg) |> Some
+
+    interface IVimBufferCreationListener with
+        member x.VimBufferCreated buffer = x.OnVimBufferCreated buffer
+
+    interface IChangeTracker with
+        member x.LastChange = _last
