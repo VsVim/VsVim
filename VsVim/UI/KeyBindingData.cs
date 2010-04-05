725e0ddc5a46cf4618b84a90c2e14e763141de64
Jared Parsons <jaredpparsons@gmail.com>
2010-04-04 20:53:40 -0700

    Converted KeyBindingData to use DependencyProperty's to facilitate data binding

--- VsVim/UI/KeyBindingData.cs
+++ VsVim/UI/KeyBindingData.cs
@@ -2,14 +2,54 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Windows;
 
 namespace VsVim.UI
 {
-    public sealed class KeyBindingData
+    public sealed class KeyBindingData :DependencyObject
     {
-        public string Name { get; set; }
-        public string Keys { get; set; }
-        public Guid CommandId { get; set; }
-        public bool IsChecked { get; set; }
+        public static readonly DependencyProperty NameProperty = DependencyProperty.Register(
+            "Name",
+            typeof(string),
+            typeof(KeyBindingData));
+
+        public static readonly DependencyProperty KeysProperty = DependencyProperty.Register(
+            "Keys",
+            typeof(string),
+            typeof(KeyBindingData));
+
+        public static readonly DependencyProperty CommandIdProperty = DependencyProperty.Register(
+            "CommandId",
+            typeof(Guid),
+            typeof(KeyBindingData));
+
+        public static readonly DependencyProperty IsCheckedProperty = DependencyProperty.Register(
+            "IsChecked",
+            typeof(bool),
+            typeof(KeyBindingData));
+
+        public string Name
+        {
+            get { return (string)GetValue(NameProperty); }
+            set { SetValue(NameProperty, value); }
+        }
+
+        public string Keys
+        {
+            get { return (string)GetValue(KeysProperty); }
+            set { SetValue(KeysProperty, value); }
+        }
+
+        public Guid CommandId
+        {
+            get { return (Guid)GetValue(CommandIdProperty); }
+            set { SetValue(CommandIdProperty, value); }
+        }
+
+        public bool IsChecked
+        {
+            get { return (bool)GetValue(IsCheckedProperty); }
+            set { SetValue(IsCheckedProperty, value); }
+        }
     }
 }
