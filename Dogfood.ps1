698e976a11ee18b3a10873324ccb240f260ad508
Jared Parsons <jaredpparsons@gmail.com>
2012-01-24 20:50:09 -0800

    Added a powershell scripts that automates the process of installing a dogfood build of VsVim based on the latest successful build

---
+++ Dogfood.ps1
@@ -0,0 +1,30 @@
+$script:scriptPath = split-path -parent $MyInvocation.MyCommand.Definition 
+
+function Get-ProgramFiles32() {
+    if ( test-path (join-path $env:WinDir "SysWow64") ) {
+        return ${env:ProgramFiles(x86)}
+    }
+    
+    return $env:ProgramFiles
+}
+
+$idePath = join-path (Get-ProgramFiles32) "Microsoft Visual Studio 10.0\Common7\IDE"
+$vsixInstaller = join-path $idePath "vsixInstaller.exe"
+$devenv = join-path $idePath "devenv.exe"
+
+if (-not (test-path $vsixInstaller)) {
+    write-error "Couldn't find $vsixInstaller"
+    return
+}
+
+gps devenv -ErrorAction SilentlyContinue | kill
+& $vsixInstaller /quiet /uninstall:VsVim.Microsoft.e214908b-0458-4ae2-a583-4310f29687c3
+
+$target = resolve-path ~\Dogfood
+mkdir $target -ErrorAction SilentlyContinue
+pushd $scriptPath
+copy VsVim\bin\Debug\* $target
+popd
+& $vsixInstaller /quiet (join-path $target "VsVim.vsix")
+wait-process "vsixInstaller"
+& $devenv
