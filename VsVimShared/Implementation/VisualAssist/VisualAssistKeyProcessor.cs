3bc897139b724e6c9eae1663daa03e888bc89164
Jared Parsons <jaredpparsons@gmail.com>
2012-04-16 16:37:25 -0700

    Raw fix for the double escape problem Visual Assist users were having. Now exiting insert mode with intellisense active only takes on Escape sequence.
    The fix is very raw and needs work.  I need to get a better handle on
    the correct way to do the PInvoke signatures I added.  Likely they are
    correct but I need to investigate a bit

---
+++ VsVimShared/Implementation/VisualAssist/VisualAssistKeyProcessor.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Diagnostics;
+using Vim;
+using Vim.Extensions;
+
+namespace VsVim.Implementation.VisualAssist
+{
+    internal sealed class VisualAssistKeyProcessor
+    {
+        private readonly IVim _vim;
+        private readonly ISharedService _sharedService;
+
+        /// <summary>
+        /// Need to save a copy of this to keep it from being garbage collected.  If it's unreference it
+        /// will be collected and the native thunk will be invalidated
+        /// </summary>
+        private readonly NativeMethods.WindowProcCallback _windowProcCallback;
+        private IntPtr _previousWndProc;
+
+        private VisualAssistKeyProcessor(
+            IVim vim,
+            ISharedService sharedService)
+        {
+            _vim = vim;
+            _sharedService = sharedService;
+            _windowProcCallback = new NativeMethods.WindowProcCallback(WindowProc);
+        }
+
+        /// <summary>
+        /// The escape key was pressed.  If we are currently in insert mode we need to leave it because it 
+        /// means that Visual Assist swallowed the key stroke
+        /// </summary>
+        private void HandleEscape()
+        {
+            var textViewOption = _sharedService.GetFocusedTextView();
+            if (textViewOption.IsNone())
+            {
+                return;
+            }
+
+            var vimBufferOption = _vim.GetVimBuffer(textViewOption.Value);
+            if (vimBufferOption.IsNone())
+            {
+                return;
+            }
+
+            var vimBuffer = vimBufferOption.Value;
+            if (vimBuffer.ModeKind == ModeKind.Insert)
+            {
+                vimBuffer.Process(KeyInputUtil.EscapeKey);
+            }
+        }
+
+        /// <summary>
+        /// Look for the Escape key going up.  Visual Assist swallows the downward stroke so we 
+        /// look for the up version
+        /// </summary>
+        private int WindowProc(IntPtr hwnd, uint msg, IntPtr wParam, IntPtr lParam)
+        {
+            if (msg == NativeMethods.WM_KEYUP && wParam != IntPtr.Zero)
+            {
+                var wParamValue = (uint)(int)(wParam);
+                if (wParamValue == NativeMethods.VK_ESCAPE)
+                {
+                    HandleEscape();
+                }
+            }
+
+            return NativeMethods.CallWindowProc(_previousWndProc, hwnd, msg, wParam, lParam);
+        }
+
+        internal static bool TryCreate(IVim vim, ISharedService sharedService, out VisualAssistKeyProcessor visualAssistKeyProcessor)
+        {
+            visualAssistKeyProcessor = null;
+
+            var process = Process.GetCurrentProcess();
+            var mainWindowHandle = process.MainWindowHandle;
+            if (mainWindowHandle == IntPtr.Zero)
+            {
+                return false;
+            }
+
+            var keyProcessor = new VisualAssistKeyProcessor(vim, sharedService);
+            keyProcessor._previousWndProc = (IntPtr)NativeMethods.SetWindowLong(process.MainWindowHandle, NativeMethods.GWLP_WNDPROC, keyProcessor._windowProcCallback);
+            if (keyProcessor._previousWndProc == IntPtr.Zero)
+            {
+                return false;
+            }
+
+            visualAssistKeyProcessor = keyProcessor;
+            return true;
+        }
+    }
+}
---
+++ VsVimShared/Implementation/VisualAssist/VisualAssistListener.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.ComponentModel.Composition;
+using System.Windows.Threading;
+using Microsoft.VisualStudio.Editor;
+using Microsoft.VisualStudio.Shell;
+using Microsoft.VisualStudio.Shell.Interop;
+using Microsoft.VisualStudio.Text.Editor;
+using Microsoft.VisualStudio.TextManager.Interop;
+using Microsoft.VisualStudio.Utilities;
+using Vim;
+using Vim.UI.Wpf;
+
+namespace VsVim.Implementation.VisualAssist
+{
+    // TODO: Clean up the native method code.  Need to use HandleRef and make sure I have IntPtr vs. int correct
+    // TODO: Need to coordinate the key handling with IVimBufferCoordinator
+    [ContentType(Vim.Constants.ContentType)]
+    [TextViewRole(PredefinedTextViewRoles.Document)]
+    [Export(typeof(IVsTextViewCreationListener))]
+    internal sealed class VisualAssistListener : IVsTextViewCreationListener
+    {
+        private static readonly Guid VisualAssistPackageId = new Guid("{44630d46-96b5-488c-8df9-26e21db8c1a3}");
+
+        private readonly ISharedService _sharedService;
+        private readonly IProtectedOperations _protectedOperations;
+        private readonly IVim _vim;
+        private readonly bool _isVisualAssistInstalled;
+        private bool _subclassedMainWindow;
+
+        /// <summary>
+        /// Reference to the key processor must be strongly held.  If it's not held then it will be garbage collected
+        /// and take it's PInvoke callback with it.  This will cause the native thunk to be invalid
+        /// </summary>
+        private VisualAssistKeyProcessor _visualAssistKeyProcessor;
+
+        [ImportingConstructor]
+        internal VisualAssistListener(
+            SVsServiceProvider serviceProvider,
+            IVim vim,
+            ISharedServiceFactory sharedServiceFactory,
+            IProtectedOperations protectedOperations)
+        {
+            _vim = vim;
+            _sharedService = sharedServiceFactory.Create();
+            _protectedOperations = protectedOperations;
+
+            var vsShell = serviceProvider.GetService<SVsShell, IVsShell>();
+            _isVisualAssistInstalled = vsShell.IsPackageInstalled(VisualAssistPackageId);
+        }
+
+        private void SubclassMainWindow()
+        {
+            try
+            {
+                VisualAssistKeyProcessor.TryCreate(_vim, _sharedService, out _visualAssistKeyProcessor);
+            }
+            finally
+            {
+                _subclassedMainWindow = true;
+            }
+        }
+
+        void IVsTextViewCreationListener.VsTextViewCreated(IVsTextView textViewAdapter)
+        {
+            if (!_isVisualAssistInstalled || _subclassedMainWindow)
+            {
+                return;
+            }
+
+            _protectedOperations.BeginInvoke(SubclassMainWindow, DispatcherPriority.Background);
+        }
+    }
+}
