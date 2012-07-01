4833f538f56435aa6c6f6ab7c5d62dad6e8a336e
Jared Parsons <jaredpparsons@gmail.com>
2012-07-01 11:10:41 -0700

    Fixed cursorline syncronization
    On startup Visual Studio's cursorline setting was taking priority over
    the Vim setting.  This only showed up in 2012 though because it
    defaulted to the cursor line being on.  The Vim setting now wins out.

--- VimWpf/Implementation/SettingSynchronizer.cs
+++ VimWpf/Implementation/SettingSynchronizer.cs
@@ -1,4 +1,6 @@
-﻿using System.ComponentModel.Composition;
+﻿using System;
+using System.ComponentModel.Composition;
+using System.Windows.Threading;
 using Microsoft.VisualStudio.Text.Editor;
 
 namespace Vim.UI.Wpf.Implementation
@@ -9,18 +11,43 @@ namespace Vim.UI.Wpf.Implementation
     [Export(typeof(IVimBufferCreationListener))]
     internal sealed class SettingSynchronizer : IVimBufferCreationListener
     {
-        private void OnSettingChanged(IVimBuffer buffer, SettingEventArgs args)
+        IProtectedOperations _protectedOperations;
+
+        [ImportingConstructor]
+        internal SettingSynchronizer(IProtectedOperations protectedOperations)
+        {
+            _protectedOperations = protectedOperations;
+        }
+
+        private void OnSettingChanged(IVimBuffer vimBuffer, SettingEventArgs args)
         {
             var setting = args.Setting;
-            if (setting.Name == WindowSettingNames.CursorLineName && buffer.TextView.Options != null)
+            if (setting.Name == WindowSettingNames.CursorLineName)
             {
-                buffer.TextView.Options.SetOptionValue(DefaultWpfViewOptions.EnableHighlightCurrentLineId, buffer.WindowSettings.CursorLine);
+                SyncVimToEditor(vimBuffer);
             }
+        }
+
+        private void SyncVimToEditor(IVimBuffer vimBuffer)
+        {
+            var options = vimBuffer.TextView.Options;
+            if (options == null)
+            {
+                return;
+            }
+
+            options.SetOptionValue(DefaultWpfViewOptions.EnableHighlightCurrentLineId, vimBuffer.WindowSettings.CursorLine);
         }
 
         void IVimBufferCreationListener.VimBufferCreated(IVimBuffer vimBuffer)
         {
             vimBuffer.WindowSettings.SettingChanged += (_, args) => OnSettingChanged(vimBuffer, args);
+
+            // On startup enforce the vim cursor line setting over any Visual Studio default.  This is important
+            // because the default changes between Dev10 (off) and Dev11 (on).  Need to be consistent here and
+            // choose the Vim setting
+            Action action = () => SyncVimToEditor(vimBuffer);
+            _protectedOperations.BeginInvoke(action, DispatcherPriority.Loaded);
         }
     }
 }
