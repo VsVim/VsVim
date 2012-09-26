cb52175f87669fd124413d49e5df423a122d4702
Jared Parsons <jaredpparsons@gmail.com>
2012-09-26 08:22:14 -0700

    Handle clipboard exception
    Even though it's not documented the Clipboard.SetText method can throw
    an exception.  This comes when trying to flush the existing data out of
    the clipboard.  Went ahead and wrapped both the SetText and GetText
    methods to prevent unhandled exceptions
    
    Sample stack trace of the exception
    
    Exception: System.Runtime.InteropServices.COMException
    Stack Trace:
    at
    System.Runtime.InteropServices.Marshal.ThrowExceptionForHRInternal(Int32
    errorCode, IntPtr errorInfo)
    at System.Runtime.InteropServices.Marshal.ThrowExceptionForHR(Int32
    errorCode, IntPtr errorInfo)
    at System.Windows.Clipboard.Flush()
    at System.Windows.Clipboard.CriticalSetDataObject(Object data, Boolean
    copy)
    at System.Windows.Clipboard.SetDataInternal(String format, Object data)
    at System.Windows.Clipboard.SetText(String text, TextDataFormat format)
    at System.Windows.Clipboard.SetText(String text)
    at Vim.UI.Wpf.Implementation.Misc.ClipboardDevice.set_Text(String value)
    at
    Vim.ClipboardRegisterValueBacking.Vim-IRegisterValueBacking-set_RegisterValue(RegisterValue
    value)
    at Vim.Register.set_RegisterValue(RegisterValue value)
    at Vim.RegisterMap.SetRegisterValue(Register reg, RegisterOperation
    regOperation, RegisterValue value)
    at Vim.RegisterMap.Vim-IRegisterMap-SetRegisterValue(Register register,
    RegisterOperation operation, RegisterValue value)
    at Vim.CommandUtil.YankSelection(Register register, VisualSpan
    visualSpan)
    at Vim.CommandUtil.RunVisualCommand(VisualCommand command, CommandData
    data, VisualSpan visualSpan)
    at Vim.CommandUtil.RunCommand(Command command)
    at Vim.CommandUtil.Vim-ICommandUtil-RunCommand(Command command)
    at Vim.CommandRunner.Run(KeyInput ki)
    at Vim.CommandRunner.Vim-ICommandRunner-Run(KeyInput ki)
    at Vim.Modes.Visual.VisualMode.Process(KeyInput ki)
    at Vim.Modes.Visual.VisualMode.Vim-IMode-Process(KeyInput keyInput)
    at Vim.VimBuffer.ProcessOneKeyInput(KeyInput keyInput)
    at <StartupCode$Vim-Core>.$VimBuffer.processSet@387.Invoke(KeyInputSet
    keyInputSet)
    at Vim.VimBuffer.ProcessCore(KeyInputSet keyInputSet)
    at Vim.VimBuffer.Process(KeyInput keyInput)
    at Vim.VimBuffer.Vim-IVimBuffer-Process(KeyInput keyInput)
    at Vim.UI.Wpf.VimKeyProcessor.TryProcess(KeyInput keyInput)
    at VsVim.Implementation.Misc.VsKeyProcessor.TryProcess(KeyInput
    keyInput)
    at Vim.UI.Wpf.VimKeyProcessor.TextInput(TextCompositionEventArgs args)
    at
    Microsoft.VisualStudio.Text.Editor.Implementation.KeyProcessorDispatcher.<DispatchTextInputEvents>b__0(KeyProcessor
    p, TextCompositionEventArgs args)
    at
    Microsoft.VisualStudio.Text.Editor.Implementation.KeyProcessorDispatcher.<>c__DisplayClass1e`1.<Dispatch>b__1a()
    at
    Microsoft.VisualStudio.Text.Utilities.GuardedOperations.CallExtensionPoint(Object
    errorSource, Action call)

--- VimWpf/Implementation/Misc/ClipboardDevice.cs
+++ VimWpf/Implementation/Misc/ClipboardDevice.cs
@@ -6,10 +6,38 @@ namespace Vim.UI.Wpf.Implementation.Misc
     [Export(typeof(IClipboardDevice))]
     internal sealed class ClipboardDevice : IClipboardDevice
     {
-        public string Text
+        private string GetText()
         {
-            get { return Clipboard.GetText(); }
-            set { Clipboard.SetText(value); }
+            try
+            {
+                return Clipboard.GetText();
+            }
+            catch
+            {
+                return string.Empty;
+            }
         }
+
+        private void SetText(string text)
+        {
+            try
+            {
+                Clipboard.SetText(text);
+            }
+            catch
+            {
+
+            }
+        }
+
+        #region IClipboardDevice
+
+        string IClipboardDevice.Text
+        {
+            get { return GetText(); }
+            set { SetText(value); }
+        }
+
+        #endregion
     }
 }
