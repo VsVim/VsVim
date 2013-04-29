ef2df9aeae8ffbb023845ae427005710d094780b
Jared Parsons <jaredpparsons@gmail.com>
2013-04-29 15:34:27 -0700

    Dirt simple display for ^]
    Tons more work to do.  But proof of concept on how this could be done
    via adornments

---
+++ VimWpf/Implementation/CharDisplay/CharDisplayTaggerSource.cs
@@ -0,0 +1,130 @@
+﻿using EditorUtils;
+using Microsoft.VisualStudio.Text;
+using Microsoft.VisualStudio.Text.Editor;
+using Microsoft.VisualStudio.Text.Tagging;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace Vim.UI.Wpf.Implementation.CharDisplay
+{
+    internal sealed class CharDisplayTaggerSource : IBasicTaggerSource<IntraTextAdornmentTag>
+    {
+        private readonly ITextView _textView;
+        private readonly Dictionary<int, UIElement> _adornmentCache = new Dictionary<int, UIElement>();
+        private EventHandler _changedEvent;
+
+        internal CharDisplayTaggerSource(ITextView textView)
+        {
+            _textView = textView;
+            _textView.TextBuffer.Changed += OnTextBufferChanged;
+            _textView.Closed += OnTextViewClosed;
+        }
+
+        internal ReadOnlyCollection<ITagSpan<IntraTextAdornmentTag>> GetTags(SnapshotSpan span)
+        {
+            var list = new List<ITagSpan<IntraTextAdornmentTag>>();
+            GetTags(span, list);
+            return list.ToReadOnlyCollectionShallow();
+        }
+
+        private void GetTags(SnapshotSpan span, List<ITagSpan<IntraTextAdornmentTag>> list)
+        {
+            var offset = span.Start.Position;
+            var snapshot = span.Snapshot;
+            for (int i = 0; i < span.Length; i++)
+            {
+                var index = i + offset;
+                var c = snapshot[index];
+                if (!IsRelevant(c))
+                {
+                    continue;
+                }
+
+                UIElement adornment;
+                if (!_adornmentCache.TryGetValue(index, out adornment))
+                {
+                    var textBox = new TextBox();
+                    textBox.Text = "^]";
+                    textBox.Measure(new Size(double.PositiveInfinity, double.PositiveInfinity));
+                    adornment = textBox;
+                    _adornmentCache[index] = textBox;
+                }
+
+                var tag = new IntraTextAdornmentTag(adornment, null);
+                var adornmentSpan = new SnapshotSpan(snapshot, index, 1);
+                var tagSpan = new TagSpan<IntraTextAdornmentTag>(adornmentSpan, tag);
+                list.Add(tagSpan);
+            }
+        }
+
+        private void OnTextBufferChanged(object sender, TextContentChangedEventArgs e)
+        {
+            foreach (var change in e.Changes)
+            {
+                if (CheckForChange(change.OldText) || CheckForChange(change.NewText))
+                {
+                    RaiseChanged();
+                    return;
+                }
+            }
+
+            _adornmentCache.Clear();
+        }
+
+        private bool CheckForChange(string text)
+        {
+            foreach (var c in text)
+            {
+                if (IsRelevant(c))
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        private void OnTextViewClosed(object sender, EventArgs e)
+        {
+            _textView.TextBuffer.Changed -= OnTextBufferChanged;
+        }
+
+        private void RaiseChanged()
+        {
+            var e = _changedEvent;
+            if (e != null)
+            {
+                e(this, EventArgs.Empty);
+            }
+        }
+
+        private static bool IsRelevant(char c)
+        {
+            // TODO: Expand to all control characters.  For now this is just proof of concept
+            return c == 29;
+        }
+
+        #region IBasicTaggerSource<IntraTextAdornmentTag>
+
+        event EventHandler IBasicTaggerSource<IntraTextAdornmentTag>.Changed
+        {
+            add { _changedEvent += value; }
+            remove { _changedEvent -= value; }
+        }
+
+        ReadOnlyCollection<ITagSpan<IntraTextAdornmentTag>> IBasicTaggerSource<IntraTextAdornmentTag>.GetTags(SnapshotSpan span)
+        {
+            return GetTags(span);
+        }
+
+        ITextSnapshot IBasicTaggerSource<IntraTextAdornmentTag>.TextSnapshot
+        {
+            get { return _textView.TextSnapshot; }
+        }
+
+        #endregion
+    }
+}
---
+++ VimWpf/Implementation/CharDisplay/CharDisplayTaggerSourceFactory.cs
@@ -0,0 +1,68 @@
+﻿using EditorUtils;
+using Microsoft.VisualStudio.Text;
+using Microsoft.VisualStudio.Text.Editor;
+using Microsoft.VisualStudio.Text.Tagging;
+using Microsoft.VisualStudio.Utilities;
+using System.ComponentModel.Composition;
+
+namespace Vim.UI.Wpf.Implementation.CharDisplay
+{
+    [Export(typeof(IViewTaggerProvider))]
+    [ContentType(Constants.AnyContentType)]
+    [TextViewRole(PredefinedTextViewRoles.Document)]
+    [TagType(typeof(IntraTextAdornmentTag))]
+    internal sealed class CharDisplayTaggerSourceFactory : IViewTaggerProvider
+    {
+        private readonly object _key = new object();
+        private readonly ITaggerFactory _taggerFactory;
+
+        [ImportingConstructor]
+        internal CharDisplayTaggerSourceFactory([EditorUtilsImport]ITaggerFactory taggerFactory)
+        {
+            _taggerFactory = taggerFactory;
+        }
+
+        // TODO: should only do this for vim supported buffers
+
+        #region IViewTaggerProvider
+
+        ITagger<T> IViewTaggerProvider.CreateTagger<T>(ITextView textView, ITextBuffer textBuffer)
+        {
+            return _taggerFactory.CreateBasicTagger(
+                textView.Properties,
+                _key,
+                () => new CharDisplayTaggerSource(textView)) as ITagger<T>;
+        }
+
+        #endregion
+    }
+
+/*
+[<Export(typeof<IViewTaggerProvider>)>]
+[<ContentType(Constants.AnyContentType)>]
+[<TextViewRole(PredefinedTextViewRoles.Document)>]
+[<TagType(typeof<TextMarkerTag>)>]
+type HighlightIncrementalSearchTaggerProvider
+    [<ImportingConstructor>]
+    ( 
+        _vim : IVim,
+        [<EditorUtilsImport>] _taggerFactory : ITaggerFactory
+    ) = 
+
+    let _key = obj()
+
+    interface IViewTaggerProvider with 
+        member x.CreateTagger<'T when 'T :> ITag> ((textView : ITextView), textBuffer) = 
+            if textView.TextBuffer = textBuffer then
+                match _vim.GetOrCreateVimBufferForHost textView with
+                | None -> null
+                | Some vimBuffer ->
+                    let tagger = _taggerFactory.CreateAsyncTagger(textView.Properties, _key, fun () ->
+                        let wordNavigator = vimBuffer.WordNavigator
+                        let taggerSource = new HighlightSearchTaggerSource(textView, vimBuffer.GlobalSettings, _vim.VimData, _vim.VimHost)
+                        taggerSource :> IAsyncTaggerSource<HighlightSearchData , TextMarkerTag>)
+                    tagger :> obj :?> ITagger<'T>
+            else
+                null
+    */
+}
