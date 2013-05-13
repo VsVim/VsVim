bc1c00a821602b3930e590dd5af5dc6012228d3d
Jared Parsons <jaredpar@microsoft.com>
2013-05-12 17:04:36 -0700

    Version number change
    Updated the version number to 1.4.0 RC.

--- Src/VimCore/Constants.fs
+++ Src/VimCore/Constants.fs
@@ -27,10 +27,10 @@ module Constants =
 
 #if DEBUG
     [<Literal>]
-    let VersionNumber = "1.3.3.3 Debug"
+    let VersionNumber = "1.3.99.0 Debug"
 #else
     [<Literal>]
-    let VersionNumber = "1.3.3.3"
+    let VersionNumber = "1.3.99.0"
 #endif
 
 
