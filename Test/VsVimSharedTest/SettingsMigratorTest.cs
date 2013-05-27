8861d666ad242ad7b80fb9e76911605abc5f5489
Jared Parsons <jaredpar@microsoft.com>
2013-05-27 11:02:25 -0700

    Minor rework of settings code
    There used to be an odd back and forth between the settings code and
    migration code.  I changed it so that VimApplicationSettings is now in
    control of the decision to migrate.  SettingsMigrator is just a simple
    migrator now, nothing more

--- Test/VsVimSharedTest/SettingsMigratorTest.cs
+++ Test/VsVimSharedTest/SettingsMigratorTest.cs
@@ -25,6 +25,7 @@ namespace VsVim.Shared.UnitTest
         {
             _factory = factory ?? new MockRepository(MockBehavior.Strict);
             _vimApplicationSettings = _factory.Create<IVimApplicationSettings>(MockBehavior.Loose);
+            _vimApplicationSettings.SetupAllProperties();
             _legacySettings = _factory.Create<ILegacySettings>(MockBehavior.Loose);
             _legacySettings.SetupGet(x => x.RemovedBindings).Returns(EmptyBindings);
             var dte = MockObjectFactory.CreateDteWithCommands();
@@ -35,70 +36,17 @@ namespace VsVim.Shared.UnitTest
                 _legacySettings.Object);
         }
 
-        public sealed class NeedsMigrationTest : SettingsMigratorTest
-        {
-            public NeedsMigrationTest()
-            {
-            }
-
-            [Fact]
-            public void AlreadyMigrated()
-            {
-                _vimApplicationSettings.SetupGet(x => x.LegacySettingsMigrated).Returns(true);
-                Assert.False(_settingsMigrator.NeedsMigration);
-            }
-
-            [Fact]
-            public void NoLegacySettingsUsed()
-            {
-                _legacySettings.SetupGet(x => x.RemovedBindings).Returns(EmptyBindings);
-                _vimApplicationSettings.SetupGet(x => x.LegacySettingsMigrated).Returns(false);
-                Assert.False(_settingsMigrator.LegacySettingsUsed);
-                Assert.False(_settingsMigrator.NeedsMigration);
-            }
-        }
-
-        public sealed class LegacySettingsUsedTest : SettingsMigratorTest
-        {
-            [Fact]
-            public void Default()
-            {
-                Assert.False(_settingsMigrator.LegacySettingsUsed);
-            }
-
-            [Fact]
-            public void HaveUpdatedKeyBindings()
-            {
-                _legacySettings.SetupGet(x => x.HaveUpdatedKeyBindings).Returns(true);
-                Assert.True(_settingsMigrator.LegacySettingsUsed);
-            }
-
-            [Fact]
-            public void IgnoredConflictingKeyBindings()
-            {
-                _legacySettings.SetupGet(x => x.IgnoredConflictingKeyBinding).Returns(true);
-                Assert.True(_settingsMigrator.LegacySettingsUsed);
-            }
-
-            [Fact]
-            public void RemovedBindings()
-            {
-                var binding = new CommandBindingSetting();
-                var list = new List<CommandBindingSetting>(new[] { binding });
-                _legacySettings.SetupGet(x => x.RemovedBindings).Returns(list.AsReadOnly());
-                Assert.True(_settingsMigrator.LegacySettingsUsed);
-            }
-        }
-
         public sealed class DoMigrationTest : SettingsMigratorTest
         {
             [Fact]
             public void CompleteMigration()
             {
-                _vimApplicationSettings.SetupProperty(x => x.LegacySettingsMigrated);
                 var list = new ReadOnlyCollection<CommandKeyBinding>(new CommandKeyBinding[] { });
+                _legacySettings.SetupGet(x => x.HaveUpdatedKeyBindings).Returns(true);
+                _legacySettings.SetupGet(x => x.IgnoredConflictingKeyBinding).Returns(true);
                 _settingsMigrator.DoMigration(list);
-                Assert.True(_vimApplicationSettings.Object.LegacySettingsMigrated);
+                Assert.True(_vimApplicationSettings.Object.HaveUpdatedKeyBindings);
+                Assert.True(_vimApplicationSettings.Object.IgnoredConflictingKeyBinding);
             }
         }
     }
