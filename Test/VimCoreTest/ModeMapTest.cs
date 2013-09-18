VsVim/VsVim

commit 31d9222647ee8008808b8002ab64f4c4230fb81c

Firmed up the initialization of modes

Src/VimCore/VimBuffer.fs
@@ -59,44 +59,45 @@ type UninitializedMode(_vimTextBuffer : IVimTextBuffer) =
         member x.OnLeave() = ()
         member x.OnClose() = ()
 
-type internal ModeMap() = 
+type internal ModeMap
+    (
+        _vimTextBuffer : IVimTextBuffer
+    ) = 
+
     let mutable _modeMap : Map<ModeKind, IMode> = Map.empty
-    let mutable _mode : IMode option = None
-    let mutable _previousMode : IMode option = None
+    let mutable _mode = UninitializedMode(_vimTextBuffer) :> IMode
+    let mutable _previousMode = _mode
     let _modeSwitchedEvent = StandardEvent<SwitchModeEventArgs>()
 
     member x.SwitchedEvent = _modeSwitchedEvent
-    member x.Mode = Option.get _mode
+    member x.Mode = _mode
     member x.PreviousMode = _previousMode
     member x.Modes = _modeMap |> Map.toSeq |> Seq.map (fun (k,m) -> m)
     member x.SwitchMode kind arg =
         let prev = _mode
         let mode = _modeMap.Item kind
-        _mode <- Some mode
+        _mode <- mode
 
-        match prev with
-        | None ->
-            () 
-        | Some prev ->
-            prev.OnLeave()
+        prev.OnLeave()
 
-            // When switching between different visual modes we don't want to lose
-            // the previous non-visual mode value.  Commands executing in Visual mode
-            // which return a SwitchPrevious mode value expected to actually leave 
-            // Visual Mode 
-            match _previousMode with
-            | None -> 
-                _previousMode <- Some prev
-            | Some mode -> 
-                if not (VisualKind.IsAnyVisual prev.ModeKind) && not (VisualKind.IsAnyVisual mode.ModeKind) then
-                    _previousMode <- Some prev
+        // When switching between different visual modes we don't want to lose
+        // the previous non-visual mode value.  Commands executing in Visual mode
+        // which return a SwitchPrevious mode value expected to actually leave 
+        // Visual Mode 
+        if not (VisualKind.IsAnyVisual prev.ModeKind) && not (VisualKind.IsAnyVisual mode.ModeKind) then
+            _previousMode <- prev
+        elif _previousMode.ModeKind = ModeKind.Uninitialized then
+            _previousMode <- prev
 
         mode.OnEnter arg
         _modeSwitchedEvent.Trigger x (SwitchModeEventArgs(prev, mode))
         mode
+
     member x.GetMode kind = Map.find kind _modeMap
+
     member x.AddMode (mode : IMode) = 
         _modeMap <- Map.add (mode.ModeKind) mode _modeMap
+
     member x.RemoveMode (mode : IMode) = 
         _modeMap <- Map.remove mode.ModeKind _modeMap
 
@@ -118,7 +119,7 @@ type internal VimBuffer
     let _statusUtil = _vimBufferData.StatusUtil
     let _properties = PropertyCollection()
     let _bag = DisposableBag()
-    let _modeMap = ModeMap()
+    let _modeMap = ModeMap(_vimBufferData.VimTextBuffer)
     let _keyMap = _vim.KeyMap
     let mutable _processingInputCount = 0
     let mutable _isClosed = false
@@ -148,13 +149,6 @@ type internal VimBuffer
         _vimTextBuffer.SwitchedMode
         |> Observable.subscribe (fun args -> this.OnVimTextBufferSwitchedMode args.ModeKind args.ModeArgument)
         |> _bag.Add
-
-        // Setup the initial uninitialized IMode value.  Note we don't want to change the 
-        // IVimTextBuffer mode here.  This is a mode which is meant to support IVimBuffer values
-        // until they can be fully initialized
-        let uninitializedMode = UninitializedMode(_vimTextBuffer) :> IMode
-        _modeMap.AddMode uninitializedMode
-        _modeMap.SwitchMode ModeKind.Uninitialized ModeArgument.None |> ignore
 
     member x.BufferedKeyInputs =
         match _bufferedKeyInput with
@@ -560,13 +554,7 @@ type internal VimBuffer
         mode
 
     member x.SwitchPreviousMode () =
-
-        match _modeMap.PreviousMode with
-        | None ->
-            // No previous mode so this is a no-op.
-            x.Mode
-        | Some previousMode ->
-            x.SwitchMode previousMode.ModeKind ModeArgument.None
+        x.SwitchMode _modeMap.PreviousMode.ModeKind ModeArgument.None
 
     /// Simulate the KeyInput being processed.  Should not go through remapping because the caller
     /// is responsible for doing the mapping.  They are indicating the literal key was processed

Src/VimCore/CoreInterfaces.fs
@@ -3651,7 +3651,7 @@ and SwitchModeKindEventArgs
 
 and SwitchModeEventArgs 
     (
-        _previousMode : IMode option,
+        _previousMode : IMode,
         _currentMode : IMode
     ) = 
 

Test/VimCoreTest/Mock/MockVimBuffer.cs
@@ -165,7 +165,7 @@ namespace Vim.UnitTest.Mock
 
         public void RaiseSwitchedMode(IMode mode)
         {
-            RaiseSwitchedMode(new SwitchModeEventArgs(FSharpOption<IMode>.None, mode));
+            RaiseSwitchedMode(new SwitchModeEventArgs(mode, mode));
         }
 
         public void RaiseSwitchedMode(SwitchModeEventArgs args)

Test/VimCoreTest/VimBufferFactoryIntegrationTest.cs
@@ -5,77 +5,80 @@ using Vim.UnitTest.Mock;
 
 namespace Vim.UnitTest
 {
-    public sealed class VimBufferFactoryIntegrationTest : VimTestBase
+    public abstract class VimBufferFactoryIntegrationTest : VimTestBase
     {
         private readonly IVim _vim;
         private readonly IVimBufferFactory _vimBufferFactory;
 
-        public VimBufferFactoryIntegrationTest()
+        protected VimBufferFactoryIntegrationTest()
         {
             _vimBufferFactory = VimBufferFactory;
             _vim = Vim;
         }
 
-        /// <summary>
-        /// Ensure that CreateVimBuffer actually creates an IVimBuffer instance
-        /// </summary>
-        [Fact]
-        public void CreateVimBuffer_Simple()
+        public sealed class CreateVimBufferTest : VimBufferFactoryIntegrationTest
         {
-            var textView = CreateTextView("");
-            var vimTextBuffer = _vimBufferFactory.CreateVimTextBuffer(textView.TextBuffer, _vim);
-            var buffer = _vimBufferFactory.CreateVimBuffer(textView, vimTextBuffer);
-            Assert.NotNull(buffer);
-            Assert.Equal(ModeKind.Normal, buffer.ModeKind);
-            Assert.Same(vimTextBuffer, buffer.VimTextBuffer);
-        }
+            /// <summary>
+            /// Ensure that CreateVimBuffer actually creates an IVimBuffer instance
+            /// </summary>
+            [Fact]
+            public void Simple()
+            {
+                var textView = CreateTextView("");
+                var vimTextBuffer = _vimBufferFactory.CreateVimTextBuffer(textView.TextBuffer, _vim);
+                var buffer = _vimBufferFactory.CreateVimBuffer(textView, vimTextBuffer);
+                Assert.NotNull(buffer);
+                Assert.Equal(ModeKind.Normal, buffer.ModeKind);
+                Assert.Same(vimTextBuffer, buffer.VimTextBuffer);
+            }
 
-        /// <summary>
-        /// The IVimBufferFactory should be stateless and happily create multiple IVimBuffer instances for a 
-        /// given ITextView (even though at an application level that will be illegal)
-        /// </summary>
-        [Fact]
-        public void CreateVimBuffer_Stateless()
-        {
-            var textView = CreateTextView("");
-            var vimTextBuffer = _vimBufferFactory.CreateVimTextBuffer(textView.TextBuffer, _vim);
-            var buffer1 = _vimBufferFactory.CreateVimBuffer(textView, vimTextBuffer);
-            var buffer2 = _vimBufferFactory.CreateVimBuffer(textView, vimTextBuffer);
-            Assert.NotSame(buffer1, buffer2);
-        }
+            /// <summary>
+            /// The IVimBufferFactory should be stateless and happily create multiple IVimBuffer instances for a 
+            /// given ITextView (even though at an application level that will be illegal)
+            /// </summary>
+            [Fact]
+            public void Stateless()
+            {
+                var textView = CreateTextView("");
+                var vimTextBuffer = _vimBufferFactory.CreateVimTextBuffer(textView.TextBuffer, _vim);
+                var buffer1 = _vimBufferFactory.CreateVimBuffer(textView, vimTextBuffer);
+                var buffer2 = _vimBufferFactory.CreateVimBuffer(textView, vimTextBuffer);
+                Assert.NotSame(buffer1, buffer2);
+            }
 
-        /// <summary>
-        /// Create the IVimBuffer for an uninitialized ITextView instance.  This should create an 
-        /// IVimBuffer in the uninitialized state 
-        /// </summary>
-        [Fact]
-        public void CreateVimBuffer_UninitializedTextView()
-        {
-            var textBuffer = CreateTextBuffer("");
-            var textView = MockObjectFactory.CreateTextView(textBuffer);
-            textView.SetupGet(x => x.TextViewLines).Returns((ITextViewLineCollection)null);
-            var vimTextBuffer = _vimBufferFactory.CreateVimTextBuffer(textBuffer, _vim);
-            var vimBuffer = _vimBufferFactory.CreateVimBuffer(textView.Object, vimTextBuffer);
-            Assert.Equal(ModeKind.Uninitialized, vimBuffer.ModeKind);
-        }
+            /// <summary>
+            /// Create the IVimBuffer for an uninitialized ITextView instance.  This should create an 
+            /// IVimBuffer in the uninitialized state 
+            /// </summary>
+            [Fact]
+            public void UninitializedTextView()
+            {
+                var textBuffer = CreateTextBuffer("");
+                var textView = MockObjectFactory.CreateTextView(textBuffer);
+                textView.SetupGet(x => x.TextViewLines).Returns((ITextViewLineCollection)null);
+                var vimTextBuffer = _vimBufferFactory.CreateVimTextBuffer(textBuffer, _vim);
+                var vimBuffer = _vimBufferFactory.CreateVimBuffer(textView.Object, vimTextBuffer);
+                Assert.Equal(ModeKind.Uninitialized, vimBuffer.ModeKind);
+            }
 
-        /// <summary>
-        /// Once an ITextView state is initialized the IVimBuffer should move to the appropriate state
-        /// </summary>
-        [Fact]
-        public void CreateVimBuffer_TextViewDelayInitialize()
-        {
-            var textBuffer = CreateTextBuffer("");
-            var textView = MockObjectFactory.CreateTextView(textBuffer);
-            textView.SetupGet(x => x.TextViewLines).Returns((ITextViewLineCollection)null);
-            var vimTextBuffer = _vimBufferFactory.CreateVimTextBuffer(textBuffer, _vim);
-            var vimBuffer = _vimBufferFactory.CreateVimBuffer(textView.Object, vimTextBuffer);
-            Assert.Equal(ModeKind.Uninitialized, vimBuffer.ModeKind);
+            /// <summary>
+            /// Once an ITextView state is initialized the IVimBuffer should move to the appropriate state
+            /// </summary>
+            [Fact]
+            public void TextViewDelayInitialize()
+            {
+                var textBuffer = CreateTextBuffer("");
+                var textView = MockObjectFactory.CreateTextView(textBuffer);
+                textView.SetupGet(x => x.TextViewLines).Returns((ITextViewLineCollection)null);
+                var vimTextBuffer = _vimBufferFactory.CreateVimTextBuffer(textBuffer, _vim);
+                var vimBuffer = _vimBufferFactory.CreateVimBuffer(textView.Object, vimTextBuffer);
+                Assert.Equal(ModeKind.Uninitialized, vimBuffer.ModeKind);
 
-            textView.SetupGet(x => x.TextViewLines).Returns(new Mock<ITextViewLineCollection>().Object);
-            textView.Raise(x => x.LayoutChanged += null, (TextViewLayoutChangedEventArgs)null);
+                textView.SetupGet(x => x.TextViewLines).Returns(new Mock<ITextViewLineCollection>().Object);
+                textView.Raise(x => x.LayoutChanged += null, (TextViewLayoutChangedEventArgs)null);
 
-            Assert.Equal(ModeKind.Normal, vimBuffer.ModeKind);
+                Assert.Equal(ModeKind.Normal, vimBuffer.ModeKind);
+            }
         }
     }
 }

Test/VimWpfTest/CommandMarginControllerTest.cs
@@ -70,7 +70,7 @@ namespace Vim.UI.Wpf.UnitTest
             var mode = new Mock<INormalMode>();
             mode.SetupGet(x => x.ModeKind).Returns(ModeKind.Normal);
             _vimBuffer.NormalModeImpl = mode.Object;
-            _vimBuffer.RaiseSwitchedMode(new SwitchModeEventArgs(FSharpOption<IMode>.None, _vimBuffer.NormalModeImpl));
+            _vimBuffer.RaiseSwitchedMode(new SwitchModeEventArgs(_vimBuffer.NormalMode, _vimBuffer.NormalModeImpl));
             Assert.Equal(String.Empty, _marginControl.StatusLine);
         }
 
@@ -82,7 +82,7 @@ namespace Vim.UI.Wpf.UnitTest
         {
             var mode = new Mock<IMode>();
             mode.SetupGet(x => x.ModeKind).Returns(ModeKind.Insert);
-            _vimBuffer.RaiseSwitchedMode(new SwitchModeEventArgs(FSharpOption<IMode>.None, mode.Object));
+            _vimBuffer.RaiseSwitchedMode(new SwitchModeEventArgs(mode.Object, mode.Object));
             Assert.Equal(Resources.InsertBanner, _marginControl.StatusLine);
         }
 
@@ -96,7 +96,7 @@ namespace Vim.UI.Wpf.UnitTest
             mode.SetupGet(x => x.ModeKind).Returns(ModeKind.Insert);
             _marginControl.StatusLine = String.Empty;
            _vimBuffer.RaiseKeyInputStart(KeyInputUtil.CharToKeyInput('c'));
-            _vimBuffer.RaiseSwitchedMode(new SwitchModeEventArgs(FSharpOption<IMode>.None, mode.Object));
+            _vimBuffer.RaiseSwitchedMode(new SwitchModeEventArgs(mode.Object, mode.Object));
             Assert.Equal(String.Empty, _marginControl.StatusLine);
         }
 
@@ -111,7 +111,7 @@ namespace Vim.UI.Wpf.UnitTest
             var ki = KeyInputUtil.CharToKeyInput('c');
             _marginControl.StatusLine = String.Empty;
             _vimBuffer.RaiseKeyInputStart(ki);
-            _vimBuffer.RaiseSwitchedMode(new SwitchModeEventArgs(FSharpOption<IMode>.None, mode.Object));
+            _vimBuffer.RaiseSwitchedMode(new SwitchModeEventArgs(mode.Object, mode.Object));
             Assert.Equal(String.Empty, _marginControl.StatusLine);
             _vimBuffer.RaiseKeyInputEnd(ki);
             Assert.Equal(Resources.InsertBanner, _marginControl.StatusLine);

commit 59792d3099685ade7600b68048737e97e20ec470

Failing to register normal as the previous mode
A bug in the change 31d9222 caused "normal" to fail to register as the
previous mode when transitioning to any "visual" mode.  This meant that
a subsequent leave of "visual" mode via <Esc> would go to insert, or
command line, instead of normal mode.  This fixes that problem and adds
some regression tests

Src/VimCore/VimBuffer.fs
@@ -74,24 +74,24 @@ type internal ModeMap
     member x.PreviousMode = _previousMode
     member x.Modes = _modeMap |> Map.toSeq |> Seq.map (fun (k,m) -> m)
     member x.SwitchMode kind arg =
-        let prev = _mode
-        let mode = _modeMap.Item kind
-        _mode <- mode
+        let currentMode = _mode
+        let newMode = _modeMap.Item kind
+        _mode <- newMode
 
-        prev.OnLeave()
+        currentMode.OnLeave()
 
         // When switching between different visual modes we don't want to lose
         // the previous non-visual mode value.  Commands executing in Visual mode
         // which return a SwitchPrevious mode value expected to actually leave 
         // Visual Mode 
-        if not (VisualKind.IsAnyVisual prev.ModeKind) && not (VisualKind.IsAnyVisual mode.ModeKind) then
-            _previousMode <- prev
+        if not (VisualKind.IsAnyVisual currentMode.ModeKind) && not (VisualKind.IsAnyVisual _previousMode.ModeKind) then
+            _previousMode <- currentMode
         elif _previousMode.ModeKind = ModeKind.Uninitialized then
-            _previousMode <- prev
+            _previousMode <- currentMode
 
-        mode.OnEnter arg
-        _modeSwitchedEvent.Trigger x (SwitchModeEventArgs(prev, mode))
-        mode
+        newMode.OnEnter arg
+        _modeSwitchedEvent.Trigger x (SwitchModeEventArgs(currentMode, newMode))
+        newMode
 
     member x.GetMode kind = Map.find kind _modeMap
 
@@ -160,7 +160,7 @@ type internal VimBuffer
         with get() = _inOneTimeCommand
         and set value = _inOneTimeCommand <- value
     
-    /// Get the current mode
+    member x.ModeMap = _modeMap
     member x.Mode = _modeMap.Mode
     member x.NormalMode = _modeMap.GetMode ModeKind.Normal :?> INormalMode
     member x.VisualLineMode = _modeMap.GetMode ModeKind.VisualLine :?> IVisualMode

VimCore.sln
@@ -85,7 +85,4 @@ Global
 	GlobalSection(SolutionProperties) = preSolution
 		HideSolutionNode = FALSE
 	EndGlobalSection
-	GlobalSection(Performance) = preSolution
-		HasPerformanceSessions = true
-	EndGlobalSection
 EndGlobal

Test/VimCoreTest/ModeMapTest.cs
@@ -1,0 +1,71 @@
+﻿using Xunit;
+
+namespace Vim.UnitTest
+{
+    public abstract class ModeMapTest : VimTestBase
+    {
+        private readonly IVimBuffer _vimBuffer;
+        private readonly ModeMap _modeMap;
+
+        protected ModeMapTest()
+        {
+            _vimBuffer = CreateVimBuffer();
+            _modeMap = ((VimBuffer)_vimBuffer).ModeMap;
+        }
+
+        public sealed class PreviousModeTest : ModeMapTest
+        {
+            public PreviousModeTest()
+            {
+                Assert.Equal(ModeKind.Normal, _modeMap.Mode.ModeKind);
+            }
+
+            /// <summary>
+            /// Make sure that "normal" mode is properly logged as the previous mode when 
+            /// transitioning between normal -> insert -> visual 
+            /// </summary>
+            [Fact]
+            public void InsertToNormalToVisualCharacter()
+            {
+                _modeMap.SwitchMode(ModeKind.Insert, ModeArgument.None);
+                _modeMap.SwitchMode(ModeKind.Normal, ModeArgument.None);
+                _modeMap.SwitchMode(ModeKind.VisualCharacter, ModeArgument.None);
+                Assert.Equal(ModeKind.Normal, _modeMap.PreviousMode.ModeKind);
+            }
+
+            /// <summary>
+            /// When switching between visual modes we don't want to change the previous mode to a 
+            /// non-visual mode.  Instead keep the previous non-visual mode 
+            /// </summary>
+            [Fact]
+            public void VisualCharacterToVisualBlock()
+            {
+                _modeMap.SwitchMode(ModeKind.VisualCharacter, ModeArgument.None);
+                _modeMap.SwitchMode(ModeKind.VisualLine, ModeArgument.None);
+                Assert.Equal(ModeKind.Normal, _modeMap.PreviousMode.ModeKind);
+            }
+
+            [Fact]
+            public void NormalToInsertToNormal()
+            {
+                _modeMap.SwitchMode(ModeKind.Insert, ModeArgument.None);
+                _modeMap.SwitchMode(ModeKind.Normal, ModeArgument.None);
+                Assert.Equal(ModeKind.Insert, _modeMap.PreviousMode.ModeKind);
+            }
+        }
+
+        public sealed class MiscTest : ModeMapTest
+        {
+            /// <summary>
+            /// Make sure that we properly transition to normal mode when leaving visual mode 
+            /// </summary>
+            [Fact]
+            public void Issue1170()
+            {
+                _vimBuffer.ProcessNotation(@"i<Esc>v<Esc>");
+                Assert.Equal(ModeKind.Normal, _vimBuffer.ModeKind);
+            }
+        }
+    }
+}
+

Test/VimCoreTest/VimCoreTest.csproj
@@ -85,6 +85,7 @@
     <Compile Include="Exports\OutlinerTaggerProvider.cs" />
     <Compile Include="HistoryListTest.cs" />
     <Compile Include="MatchingTokenUtilTest.cs" />
+    <Compile Include="ModeMapTest.cs" />
     <Compile Include="RegisterValueTest.cs" />
     <Compile Include="SnapshotOverlapPointTest.cs" />
     <Compile Include="SnapshotOverlapSpanTest.cs" />