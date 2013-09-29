d03121531fa7ecddac3ad427c017cd118ed7fc05
Jared Parsons <jaredpar@microsoft.com>
2013-09-29 16:50:09 -0700

    Solution for all Visual Studio versions
    Added a solution which works for all visual studio versions.  May still
    need to do some work to get everything 100% going.  But this is the best
    path forward

--- Src/VsVimShared/Extensions.cs
+++ Src/VsVimShared/Extensions.cs
@@ -970,33 +970,6 @@ namespace VsVim
             }
         }
 
-        public static VisualStudioVersion GetVisualStudioVersion(this _DTE dte)
-        {
-            var version = dte.Version;
-            if (string.IsNullOrEmpty(dte.Version))
-            {
-                return VisualStudioVersion.Unknown;
-            }
-
-            var parts = version.Split('.');
-            if (parts.Length == 0)
-            {
-                return VisualStudioVersion.Unknown;
-            }
-
-            switch (parts[0])
-            {
-                case "10":
-                    return VisualStudioVersion.Vs2010;
-                case "11":
-                    return VisualStudioVersion.Vs2012;
-                case "12":
-                    return VisualStudioVersion.Vs2013;
-                default:
-                    return VisualStudioVersion.Unknown;
-            }
-        }
-
         #endregion
 
         #region Project
