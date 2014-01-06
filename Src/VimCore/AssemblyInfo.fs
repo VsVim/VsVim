bba17b1dd44cd3d4b82930c0a14720d959040b79
Jared Parsons <Jared Parsons>
2014-01-06 15:09:30 -0800

    Fixed build for Roslyn

--- Src/VimCore/AssemblyInfo.fs
+++ Src/VimCore/AssemblyInfo.fs
@@ -5,7 +5,9 @@ namespace Vim
 
 open System.Runtime.CompilerServices
 
+[<assembly:Extension()>]
 [<assembly:InternalsVisibleTo("Vim.Core.UnitTest")>]
 [<assembly:InternalsVisibleTo("Vim.UnitTest.Utils")>]
 [<assembly:InternalsVisibleTo("DynamicProxyGenAssembly2")>] // Moq
 do()
+
