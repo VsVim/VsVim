50e0c4eb5dc5de2c9c632b015d5c2a115726b68e
Jared Parsons <jaredpar@microsoft.com>
2014-02-19 16:05:34 -0800

    Package loading
    This change creates a package and gets it loading on F5.  Still tons of
    work to do but this is a simple first step that we can build on

---
+++ Src/VsVim/Guids.cs
@@ -0,0 +1,16 @@
+﻿// Guids.cs
+// MUST match guids.h
+using System;
+
+namespace Company.VsVim
+{
+    static class GuidList
+    {
+        public const string guidVsVimPkgString = "a284d12c-1e96-451b-a3b0-5486a1beb6ca";
+        public const string guidVsVimCmdSetString = "c7509f48-7d69-4344-8221-01989a8c4be5";
+
+        public static readonly Guid guidVsVimCmdSet = new Guid(guidVsVimCmdSetString);
+    };
+
+
+}
---
+++ Src/VsVim/PkgCmdID.cs
@@ -0,0 +1,13 @@
+﻿// PkgCmdID.cs
+// MUST match PkgCmdID.h
+using System;
+
+namespace Company.VsVim
+{
+    static class PkgCmdIDList
+    {
+        public const uint cmdidMyCommand =        0x100;
+
+
+    };
+}
