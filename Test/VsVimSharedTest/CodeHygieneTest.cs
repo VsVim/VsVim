0389b498c396992ab48d483f352f2ca87b2d7104
Jared Parsons <jaredpar@microsoft.com>
2014-07-21 17:12:18 -0700

    Updated code hygiene tests

--- Test/VsVimSharedTest/CodeHygieneTest.cs
+++ Test/VsVimSharedTest/CodeHygieneTest.cs
@@ -15,12 +15,35 @@ namespace Vim.VisualStudio.UnitTest
         private readonly Assembly _assembly = typeof(CodeHygieneTest).Assembly;
 
         [Fact]
-        public void Namespace()
+        public void TestNamespace()
         {
             const string prefix = "Vim.VisualStudio.Shared.UnitTest.";
             foreach (var type in _assembly.GetTypes().Where(x => x.IsPublic))
             {
                 Assert.True(type.FullName.StartsWith(prefix, StringComparison.Ordinal), String.Format("Wrong namespace prefix on {0}", type.FullName));
+            }
+        }
+
+        [Fact]
+        public void CodeNamespace()
+        {
+            const string prefix = "Vim.VisualStudio.";
+            var assemblies = new[]
+                {
+                    typeof(ISharedService).Assembly,
+                    typeof(IVsAdapter).Assembly
+                };
+            foreach (var assembly in assemblies)
+            {
+                foreach (var type in assembly.GetTypes())
+                {
+                    if (type.FullName.StartsWith("Xaml", StringComparison.Ordinal))
+                    {
+                        continue;
+                    }
+
+                    Assert.True(type.FullName.StartsWith(prefix, StringComparison.Ordinal), String.Format("Wrong namespace prefix on {0}", type.FullName));
+                }
             }
         }
     }
