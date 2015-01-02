119472d4cc7264e1f29c606d83cede05cf69cc18
Jared Parsons <jaredpar@microsoft.com>
2015-01-02 13:51:42 -0500

    Regression suite
    Regression suite for #1522

--- Test/VimCoreTest/NormalModeIntegrationTest.cs
+++ Test/VimCoreTest/NormalModeIntegrationTest.cs
@@ -560,6 +560,36 @@ namespace Vim.UnitTest
                 _vimBuffer.Process('*');
                 Assert.Equal(_textBuffer.GetLine(0).Start, _textView.GetCaretPoint());
             }
+
+            /// <summary>
+            /// Have to make sure that 'j' correctly maintains caret column when stepping 
+            /// over collapsed regions.  
+            ///
+            /// In general this is straight forward because vim regions include the trailing
+            /// new line.  A C# region though does not and we end up with the text of 2 
+            /// lines (first and new line of last) in the same visual line.  This makes mapping
+            /// considerably more difficult.  
+            /// </summary>
+            [Fact(Skip= "Disabled while fixing 1522")]
+            public void Issue1522()
+            {
+                Create("cat", "dog", "bear", "tree");
+
+                // The span should *not* include the line break of the last line.  
+                var span = new SnapshotSpan(
+                    _textBuffer.GetLine(1).Start.Add(2),
+                    _textBuffer.GetLine(2).End);
+
+                // Collapse the region specified above
+                var adhocOutliner = EditorUtilsFactory.GetOrCreateOutliner(_textBuffer);
+                adhocOutliner.CreateOutliningRegion(span, SpanTrackingMode.EdgeInclusive, "test", "test");
+                OutliningManagerService.GetOutliningManager(_textView).CollapseAll(span, _ => true);
+
+                _vimBuffer.ProcessNotation("j");
+                Assert.Equal(_textBuffer.GetLine(1).Start, _textView.GetCaretPoint());
+                _vimBuffer.ProcessNotation("j");
+                Assert.Equal(_textBuffer.GetLine(3).Start, _textView.GetCaretPoint());
+            }
         }
 
         public sealed class MatchingTokenTest : NormalModeIntegrationTest
