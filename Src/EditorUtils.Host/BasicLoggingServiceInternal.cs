22f75a8877ec6dedc726bcf9cf02081754aba023
Jared Parsons <jaredpar@microsoft.com>
2017-10-03 21:10:50 -0700

    Can run test now on 2017

---
+++ Src/EditorUtils.Host/BasicLoggingServiceInternal.cs
@@ -0,0 +1,45 @@
+﻿#if VS2017
+using Microsoft.VisualStudio.Telemetry;
+using Microsoft.VisualStudio.Text.Utilities;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.Composition;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EditorUtils.Vs2017
+{
+    [Export(typeof(ILoggingServiceInternal))]
+    internal sealed class BasicLoggingServiceInternal : ILoggingServiceInternal
+    {
+        void ILoggingServiceInternal.AdjustCounter(string key, string name, int delta)
+        {
+        }
+
+        void ILoggingServiceInternal.PostCounters()
+        {
+        }
+
+        void ILoggingServiceInternal.PostEvent(string key, params object[] namesAndProperties)
+        {
+        }
+
+        void ILoggingServiceInternal.PostEvent(string key, IReadOnlyList<object> namesAndProperties)
+        {
+        }
+
+        void ILoggingServiceInternal.PostEvent(DataModelEventType eventType, string eventName, TelemetryResult result, params (string name, object property)[] namesAndProperties)
+        {
+        }
+
+        void ILoggingServiceInternal.PostEvent(DataModelEventType eventType, string eventName, TelemetryResult result, IReadOnlyList<(string name, object property)> namesAndProperties)
+        {
+        }
+
+        void ILoggingServiceInternal.PostFault(string eventName, string description, Exception exceptionObject, string additionalErrorInfo, bool? isIncludedInWatsonSample)
+        {
+        }
+    }
+}
+#endif
