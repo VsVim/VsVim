de0844756bcf711a5873c7ed94b98989914a77a5
Jared Parsons <jaredpar@microsoft.com>
2017-11-24 14:17:51 -0500

    Use auto-property

--- Src/VimEditorHost/EditorHost.cs
+++ Src/VimEditorHost/EditorHost.cs
@@ -25,102 +25,37 @@ namespace Vim.EditorHost
     /// </summary>
     public class EditorHost
     {
-        private CompositionContainer _compositionContainer;
-        private ITextBufferFactoryService _textBufferFactoryService;
-        private ITextEditorFactoryService _textEditorFactoryService;
-        private IProjectionBufferFactoryService _projectionBufferFactoryService;
-        private ISmartIndentationService _smartIndentationService;
-        private IEditorOperationsFactoryService _editorOperationsFactoryService;
-        private IEditorOptionsFactoryService _editorOptionsFactoryService;
-        private IOutliningManagerService _outliningManagerService;
-        private ITextSearchService _textSearchService;
-        private ITextBufferUndoManagerProvider _textBufferUndoManagerProvider;
-        private IContentTypeRegistryService _contentTypeRegistryService;
-        private IBasicUndoHistoryRegistry _basicUndoHistoryRegistry;
-        private IClassificationTypeRegistryService _classificationTypeRegistryService;
-
-        public CompositionContainer CompositionContainer
-        {
-            get { return _compositionContainer; }
-        }
-
-        public ISmartIndentationService SmartIndentationService
-        {
-            get { return _smartIndentationService; }
-        }
-
-        public ITextBufferFactoryService TextBufferFactoryService
-        {
-            get { return _textBufferFactoryService; }
-        }
-
-        public ITextEditorFactoryService TextEditorFactoryService
-        {
-            get { return _textEditorFactoryService; }
-        }
-
-        public IProjectionBufferFactoryService ProjectionBufferFactoryService
-        {
-            get { return _projectionBufferFactoryService; }
-        }
-
-        public IEditorOperationsFactoryService EditorOperationsFactoryService
-        {
-            get { return _editorOperationsFactoryService; }
-        }
-
-        public IEditorOptionsFactoryService EditorOptionsFactoryService
-        {
-            get { return _editorOptionsFactoryService; }
-        }
-
-        public ITextSearchService TextSearchService
-        {
-            get { return _textSearchService; }
-        }
-
-        public ITextBufferUndoManagerProvider TextBufferUndoManagerProvider
-        {
-            get { return _textBufferUndoManagerProvider; }
-        }
-
-        public IOutliningManagerService OutliningManagerService
-        {
-            get { return _outliningManagerService; }
-        }
-
-        public IContentTypeRegistryService ContentTypeRegistryService
-        {
-            get { return _contentTypeRegistryService; }
-        }
-
-        public IBasicUndoHistoryRegistry BasicUndoHistoryRegistry
-        {
-            get { return _basicUndoHistoryRegistry; }
-        }
-
-        public IClassificationTypeRegistryService ClassificationTypeRegistryService
-        {
-            get { return _classificationTypeRegistryService; }
-        }
+        public CompositionContainer CompositionContainer { get; }
+        public ISmartIndentationService SmartIndentationService { get; }
+        public ITextBufferFactoryService TextBufferFactoryService { get; }
+        public ITextEditorFactoryService TextEditorFactoryService { get; }
+        public IProjectionBufferFactoryService ProjectionBufferFactoryService { get; }
+        public IEditorOperationsFactoryService EditorOperationsFactoryService { get; }
+        public IEditorOptionsFactoryService EditorOptionsFactoryService { get; }
+        public ITextSearchService TextSearchService { get; }
+        public ITextBufferUndoManagerProvider TextBufferUndoManagerProvider { get; }
+        public IOutliningManagerService OutliningManagerService { get; }
+        public IContentTypeRegistryService ContentTypeRegistryService { get; }
+        public IBasicUndoHistoryRegistry BasicUndoHistoryRegistry { get; }
+        public IClassificationTypeRegistryService ClassificationTypeRegistryService { get; }
 
         public EditorHost(CompositionContainer compositionContainer)
         {
-            _compositionContainer = compositionContainer;
-            _textBufferFactoryService = _compositionContainer.GetExportedValue<ITextBufferFactoryService>();
-            _textEditorFactoryService = _compositionContainer.GetExportedValue<ITextEditorFactoryService>();
-            _projectionBufferFactoryService = _compositionContainer.GetExportedValue<IProjectionBufferFactoryService>();
-            _smartIndentationService = _compositionContainer.GetExportedValue<ISmartIndentationService>();
-            _editorOperationsFactoryService = _compositionContainer.GetExportedValue<IEditorOperationsFactoryService>();
-            _editorOptionsFactoryService = _compositionContainer.GetExportedValue<IEditorOptionsFactoryService>();
-            _textSearchService = _compositionContainer.GetExportedValue<ITextSearchService>();
-            _outliningManagerService = _compositionContainer.GetExportedValue<IOutliningManagerService>();
-            _textBufferUndoManagerProvider = _compositionContainer.GetExportedValue<ITextBufferUndoManagerProvider>();
-            _contentTypeRegistryService = _compositionContainer.GetExportedValue<IContentTypeRegistryService>();
-            _classificationTypeRegistryService = _compositionContainer.GetExportedValue<IClassificationTypeRegistryService>();
+            CompositionContainer = compositionContainer;
+            TextBufferFactoryService = CompositionContainer.GetExportedValue<ITextBufferFactoryService>();
+            TextEditorFactoryService = CompositionContainer.GetExportedValue<ITextEditorFactoryService>();
+            ProjectionBufferFactoryService = CompositionContainer.GetExportedValue<IProjectionBufferFactoryService>();
+            SmartIndentationService = CompositionContainer.GetExportedValue<ISmartIndentationService>();
+            EditorOperationsFactoryService = CompositionContainer.GetExportedValue<IEditorOperationsFactoryService>();
+            EditorOptionsFactoryService = CompositionContainer.GetExportedValue<IEditorOptionsFactoryService>();
+            TextSearchService = CompositionContainer.GetExportedValue<ITextSearchService>();
+            OutliningManagerService = CompositionContainer.GetExportedValue<IOutliningManagerService>();
+            TextBufferUndoManagerProvider = CompositionContainer.GetExportedValue<ITextBufferUndoManagerProvider>();
+            ContentTypeRegistryService = CompositionContainer.GetExportedValue<IContentTypeRegistryService>();
+            ClassificationTypeRegistryService = CompositionContainer.GetExportedValue<IClassificationTypeRegistryService>();
 
-            var errorHandlers = _compositionContainer.GetExportedValues<IExtensionErrorHandler>();
-            _basicUndoHistoryRegistry = _compositionContainer.GetExportedValue<IBasicUndoHistoryRegistry>();
+            var errorHandlers = CompositionContainer.GetExportedValues<IExtensionErrorHandler>();
+            BasicUndoHistoryRegistry = CompositionContainer.GetExportedValue<IBasicUndoHistoryRegistry>();
         }
 
         /// <summary>
@@ -128,7 +63,7 @@ namespace Vim.EditorHost
         /// </summary>
         public ITextBuffer CreateTextBuffer(params string[] lines)
         {
-            return _textBufferFactoryService.CreateTextBuffer(lines);
+            return TextBufferFactoryService.CreateTextBuffer(lines);
         }
 
         /// <summary>
@@ -136,7 +71,7 @@ namespace Vim.EditorHost
         /// </summary>
         public ITextBuffer CreateTextBuffer(IContentType contentType, params string[] lines)
         {
-            return _textBufferFactoryService.CreateTextBuffer(contentType, lines);
+            return TextBufferFactoryService.CreateTextBuffer(contentType, lines);
         }
 
         /// <summary>
@@ -164,13 +99,13 @@ namespace Vim.EditorHost
         public IWpfTextView CreateTextView(params string[] lines)
         {
             var textBuffer = CreateTextBuffer(lines);
-            return _textEditorFactoryService.CreateTextView(textBuffer);
+            return TextEditorFactoryService.CreateTextView(textBuffer);
         }
 
         public IWpfTextView CreateTextView(IContentType contentType, params string[] lines)
         {
-            var textBuffer = _textBufferFactoryService.CreateTextBuffer(contentType, lines);
-            return _textEditorFactoryService.CreateTextView(textBuffer);
+            var textBuffer = TextBufferFactoryService.CreateTextBuffer(contentType, lines);
+            return TextEditorFactoryService.CreateTextView(textBuffer);
         }
 
         /// <summary>
