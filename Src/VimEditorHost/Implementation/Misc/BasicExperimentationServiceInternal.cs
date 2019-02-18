dc2ab110f3b84b35a67de39b499170b3252c3398
Jared Parsons <jaredpparsons@gmail.com>
2019-02-17 17:58:51 -0800

    PR feedback

--- Src/VimEditorHost/Implementation/Misc/BasicExperimentationServiceInternal.cs
+++ Src/VimEditorHost/Implementation/Misc/BasicExperimentationServiceInternal.cs
@@ -8,6 +8,12 @@ using Microsoft.VisualStudio.Text.Utilities;
 
 namespace Vim.EditorHost.Implementation.Misc
 {
+    /// <summary>
+    /// This interface is unconditionally imported by the core editor hence we must provide an export
+    /// in order to enable testing. The implementation is meant to control A/B testing on features 
+    /// in the editor. The only documentation on this interface is provided in the discussion 
+    /// here: https://github.com/dotnet/roslyn/issues/27428
+    /// </summary>
     [Export(typeof(IExperimentationServiceInternal))]
     internal sealed class BasicExperimentationServiceInternal : IExperimentationServiceInternal
     {
