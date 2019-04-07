d5d84cf89004664378410fb3fad0c8469159c8a6
Jared Parsons <jaredpparsons@gmail.com>
2019-04-07 16:04:29 -0700

    Fleshed out TestableWritableSettingStore

---
+++ Test/VsVimSharedTest/CachedWritableSettingsStoreTest.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.VisualStudio.Settings;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Vim.VisualStudio.UnitTest
+{
+
+}
