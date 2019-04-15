0a76dae3c8079925f2830473c3921bd6ec9df232
Rick Sladkey <rick.sladkey@gmail.com>
2019-04-15 01:05:14 -0400

    Add caret position tests for 'dG' and 'dd'

--- Test/VimCoreTest/NormalModeIntegrationTest.cs
+++ Test/VimCoreTest/NormalModeIntegrationTest.cs
@@ -9058,6 +9058,50 @@ namespace Vim.UnitTest
             }
 
             /// <summary>
+            /// Deleting to the end of the file should move the caret up
+            /// </summary>
+            [WpfFact]
+            public void DeleteLines_ToEndOfFile()
+            {
+                // Reported in issue #2477.
+                Create("cat", "dog", "fish", "");
+                _textView.MoveCaretToLine(1, 0);
+                _vimBuffer.Process("dG");
+                Assert.Equal(new[] { "cat", "" }, _textBuffer.GetLines());
+                Assert.Equal(_textView.GetPointInLine(0, 0), _textView.GetCaretPoint());
+            }
+
+            /// <summary>
+            /// Deleting lines should obey the 'startofline' setting
+            /// </summary>
+            [WpfFact]
+            public void DeleteLines_StartOfLine()
+            {
+                // Reported in issue #2477.
+                Create(" cat", "  dog", " fish", "");
+                _textView.MoveCaretToLine(1, 2);
+                _vimBuffer.Process("dd");
+                Assert.Equal(new[] { " cat", " fish", "" }, _textBuffer.GetLines());
+                Assert.Equal(_textView.GetPointInLine(1, 1), _textView.GetCaretPoint());
+            }
+
+            /// <summary>
+            /// Deleting lines should preserve spaces to caret when
+            /// 'nostartofline' is in effect
+            /// </summary>
+            [WpfFact]
+            public void DeleteLines_NoStartOfLine()
+            {
+                // Reported in issue #2477.
+                Create(" cat", "  dog", " fish", "");
+                _globalSettings.StartOfLine = false;
+                _textView.MoveCaretToLine(1, 2);
+                _vimBuffer.Process("dd");
+                Assert.Equal(new[] { " cat", " fish", "" }, _textBuffer.GetLines());
+                Assert.Equal(_textView.GetPointInLine(1, 2), _textView.GetCaretPoint());
+            }
+
+            /// <summary>
             /// Subtract a negative decimal number
             /// </summary>
             [WpfFact]
