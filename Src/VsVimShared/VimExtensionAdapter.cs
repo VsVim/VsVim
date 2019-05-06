d622cab1d4e05d965ec05155de73aa0d05079f6d
Rick Sladkey <rick.sladkey@gmail.com>
2019-05-06 01:35:36 -0400

    Remove unnecessary usings

--- Src/VsVimShared/VimExtensionAdapter.cs
+++ Src/VsVimShared/VimExtensionAdapter.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Microsoft.VisualStudio.Text.Editor;
+﻿using Microsoft.VisualStudio.Text.Editor;
 
 namespace Vim.VisualStudio
 {
