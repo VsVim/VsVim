5a5ee04bfa7fd1952785e53875ec6c5951456e51
Jared Parsons <jaredpparsons@gmail.com>
2019-05-23 22:41:24 -0700

    Shutdown Dispatcher properly in tests
    This change uses the `ITestFramework` hook in xUnit to allow for
    graceful shutdown of the `Dispatcher` instance used to run our tests.
    The `ITestFramework.Dispose` method is both
    
    1. Called before `AppDomain.Unload`
    1. Called after all tests have completed executing
    
    This means it's the ideal place to tear down the `Dispatcher` and STA
    thread created to run the tests.
    
    Other places considered for tear down:
    
    1. `AppDomain.CurrentDomain.DomainUnload`: this event fires as the
    `AppDomain` is being torn down meaning the `Dispatcher` or its child
    objects could have already been finalized. Indeed this is the case on
    most of the machines tested. This meant calling
    `Dispatcher.BeginInvokeShutdown` was unreliable as the underlying HWND
    handles may have been finalized.
    1. `StaContext.Dispose`: this is not a part of any hook in xUnit hence
    is never actually invoked.
    
    Note: this may end up breaking source based discovery for tests in Test
    Explorer but normal build based discovery will continue working.
    
    closes #2563

---
+++ Src/VimTestUtils/Utilities/StaTestFramework.cs
@@ -0,0 +1,28 @@
+﻿using Xunit.Sdk;
+using Xunit.Abstractions;
+using System.Windows.Data;
+using System;
+using System.Linq;
+
+namespace Vim.UnitTest.Utilities
+{
+    public sealed class StaTestFramework : XunitTestFramework, IDisposable
+    {
+        public static bool IsCreated { get; private set; }
+
+        public StaTestFramework() : this(null)
+        {
+        }
+
+        public StaTestFramework(IMessageSink messageSink) : base(messageSink)
+        {
+            IsCreated = true;
+        }
+
+        void IDisposable.Dispose()
+        {
+            StaContext.Default.Dispose();
+            base.Dispose();
+        }
+    }
+}
---
+++ Src/VimTestUtils/Utilities/StaTestFrameworkTypeDiscoverer.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Collections.Concurrent;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using Vim.Extensions;
+using System.Windows.Threading;
+using System.Diagnostics;
+using Xunit.Sdk;
+using Xunit.Abstractions;
+
+namespace Vim.UnitTest.Utilities
+{
+    public sealed class StaTestFrameworkTypeDiscoverer : ITestFrameworkTypeDiscoverer
+    {
+        public StaTestFrameworkTypeDiscoverer()
+        {
+
+        }
+
+        Type ITestFrameworkTypeDiscoverer.GetTestFrameworkType(IAttributeInfo attribute) => typeof(StaTestFramework);
+    }
+}
