4b84767dbbc44b5d6ba7a0ca9e53731c787745a1
Rick Sladkey <rick.sladkey@gmail.com>
2019-07-09 01:36:02 -0400

    Scroll window when captured and outside the viewport

--- Src/VimWpf/Implementation/Mouse/VimMouseProcessor.cs
+++ Src/VimWpf/Implementation/Mouse/VimMouseProcessor.cs
@@ -1,8 +1,5 @@
 ﻿using Microsoft.VisualStudio.Text.Editor;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Windows.Input;
 using System.Windows.Threading;
 
@@ -10,9 +7,13 @@ namespace Vim.UI.Wpf.Implementation.Mouse
 {
     internal sealed class VimMouseProcessor : MouseProcessorBase
     {
+        /// <summary>
+        /// How many lines per second we should scroll when auto-scrolling
+        /// </summary>
+        private static readonly int s_linesPerSecond = 8;
+
         private readonly IVimBuffer _vimBuffer;
         private readonly IKeyboardDevice _keyboardDevice;
-        private readonly IProtectedOperations _protectedOperations;
         private readonly IWpfTextView _wpfTextView;
         private readonly MouseDevice _mouseDevice;
 
@@ -26,12 +27,11 @@ namespace Vim.UI.Wpf.Implementation.Mouse
         {
             _vimBuffer = vimBuffer;
             _keyboardDevice = keyboardDevice;
-            _protectedOperations = protectedOperations;
             _wpfTextView = (IWpfTextView)_vimBuffer.TextView;
             _mouseDevice = InputManager.Current.PrimaryMouseDevice;
             _mouseCaptured = false;
             _scrollTimer = new DispatcherTimer(
-                new TimeSpan(0, 0, 0, 0, 1000),
+                new TimeSpan(0, 0, 0, 0, 1000 / s_linesPerSecond),
                 DispatcherPriority.Normal,
                 protectedOperations.GetProtectedEventHandler(OnScrollTimer),
                 Dispatcher.CurrentDispatcher);
@@ -42,12 +42,14 @@ namespace Vim.UI.Wpf.Implementation.Mouse
             var keyInput = KeyInputUtil.ApplyKeyModifiersToKey(vimKey, _keyboardDevice.KeyModifiers);
             keyInput = KeyInputUtil.ApplyClickCount(keyInput, clickCount);
 
-            // If the user has explicitly set the mouse to be <nop> then we don't want to report this as 
-            // handled.  Otherwise it will swallow the mouse event and as a consequence disable other
-            // features that begin with a mouse click.  
+            // If the user has explicitly set the mouse to be <nop> then we
+            // don't want to report this as  handled.  Otherwise it will
+            // swallow the mouse event and as a consequence disable other
+            // features that begin with a mouse click.
             //
-            // There is really no other way for the user to opt out of mouse behavior besides mapping the 
-            // key to <nop> otherwise that would be done here.  
+            // There is really no other way for the user to opt out of mouse
+            // behavior besides mapping the  key to <nop> otherwise that would
+            // be done here.
             var keyInputSet = _vimBuffer.GetKeyInputMapping(keyInput).KeyInputSet;
             if (keyInputSet.Length > 0 && keyInputSet.KeyInputs[0].Key == VimKey.Nop)
             {
@@ -62,27 +64,28 @@ namespace Vim.UI.Wpf.Implementation.Mouse
             return false;
         }
 
-        private void TryProcessDrag(MouseEventArgs e, MouseButtonState state, VimKey vimKey)
+        private bool TryProcessDrag(MouseButtonState state, VimKey vimKey)
         {
             if (state == MouseButtonState.Pressed)
             {
-                e.Handled = TryProcess(vimKey);
+                return TryProcess(vimKey);
             }
+            return false;
         }
 
         public override void PreprocessMouseLeftButtonDown(MouseButtonEventArgs e)
         {
             // These methods get called for the entire mouse processing chain
-            // before calling PreprocessMouseDown (and there is not an equivalent
-            // for PreprocessMouseMiddleButtonDown).
+            // before calling PreprocessMouseDown (and there is not an
+            // equivalent for PreprocessMouseMiddleButtonDown).
             this.PreprocessMouseDown(e);
         }
 
         public override void PreprocessMouseRightButtonDown(MouseButtonEventArgs e)
         {
             // These methods get called for the entire mouse processing chain
-            // before calling PreprocessMouseDown (and there is not an equivalent
-            // for PreprocessMouseMiddleButtonDown).
+            // before calling PreprocessMouseDown (and there is not an
+            // equivalent for PreprocessMouseMiddleButtonDown).
             this.PreprocessMouseDown(e);
         }
 
@@ -118,7 +121,7 @@ namespace Vim.UI.Wpf.Implementation.Mouse
             {
                 case MouseButton.Left:
                     e.Handled = TryProcess(VimKey.LeftRelease);
-                    ReleaseMouseCapture();
+                    CheckReleaseMouseCapture();
                     break;
                 case MouseButton.Middle:
                     e.Handled = TryProcess(VimKey.MiddleRelease);
@@ -137,15 +140,16 @@ namespace Vim.UI.Wpf.Implementation.Mouse
 
         public override void PreprocessMouseMove(MouseEventArgs e)
         {
-            TryProcessDrag(e, e.LeftButton, VimKey.LeftDrag);
-            if (e.LeftButton == MouseButtonState.Pressed)
+            var handled = false;
+            handled |= TryProcessDrag(e.LeftButton, VimKey.LeftDrag);
+            handled |= TryProcessDrag(e.MiddleButton, VimKey.RightDrag);
+            handled |= TryProcessDrag(e.RightButton, VimKey.RightDrag);
+            handled |= TryProcessDrag(e.XButton1, VimKey.X1Drag);
+            handled |= TryProcessDrag(e.XButton2, VimKey.X2Drag);
+            if (handled)
             {
-                CheckScroll();
+                e.Handled = true;
             }
-            TryProcessDrag(e, e.MiddleButton, VimKey.RightDrag);
-            TryProcessDrag(e, e.RightButton, VimKey.RightDrag);
-            TryProcessDrag(e, e.XButton1, VimKey.X1Drag);
-            TryProcessDrag(e, e.XButton2, VimKey.X2Drag);
         }
 
         private void CaptureMouse()
@@ -158,7 +162,7 @@ namespace Vim.UI.Wpf.Implementation.Mouse
             }
         }
 
-        private void ReleaseMouseCapture()
+        private void CheckReleaseMouseCapture()
         {
             if (_mouseCaptured)
             {
@@ -168,24 +172,91 @@ namespace Vim.UI.Wpf.Implementation.Mouse
             }
         }
 
-        private void CheckScroll()
-        {
-            if (_mouseCaptured)
-            {
-            }
-        }
-
         private void OnScrollTimer(object sender, EventArgs e)
         {
-            if (!_mouseCaptured || !_wpfTextView.VisualElement.IsMouseCaptured)
+            // Don't attempt to scroll the text view during a layout.
+            if (_wpfTextView.InLayout)
+            {
+                return;
+            }
+
+            // Make sure the timer event is still relevant.
+            if (_wpfTextView.IsClosed
+                || !_wpfTextView.HasAggregateFocus
+                || !_mouseCaptured
+                || !_wpfTextView.VisualElement.IsMouseCaptured
+                || _mouseDevice.LeftButton != MouseButtonState.Pressed)
             {
                 _mouseCaptured = false;
                 _scrollTimer.IsEnabled = false;
                 return;
             }
 
-            _wpfTextView.ViewScroller.ScrollViewportVerticallyByLines(ScrollDirection.Up, 1);
+            // Check whether we should scroll the text view.
+            var mousePoint = _mouseDevice.GetPosition(_wpfTextView.VisualElement);
+            var lineHeight = _wpfTextView.LineHeight;
+            var scrollDirection = null as ScrollDirection?;
+            var lines = 1;
+            if (mousePoint.Y < 0)
+            {
+                if (!GetIsFirstLineVisible())
+                {
+                    scrollDirection = ScrollDirection.Up;
+                    lines = (int)Math.Ceiling((0 - mousePoint.Y) / lineHeight);
+                }
+            }
+            if (mousePoint.Y > _wpfTextView.ViewportHeight)
+            {
+                if (!GetIsLastLineVisible())
+                {
+                    scrollDirection = ScrollDirection.Down;
+                    lines = (int)Math.Ceiling((mousePoint.Y - _wpfTextView.ViewportHeight) / lineHeight);
+                }
+            }
+
+            // If we should scroll, scroll the text view and process a virtual
+            // drag event.
+            if (scrollDirection.HasValue)
+            {
+                _wpfTextView.ViewScroller.ScrollViewportVerticallyByLines(scrollDirection.Value, lines);
+                TryProcessDrag(MouseButtonState.Pressed, VimKey.LeftDrag);
+            }
         }
 
+        private bool GetIsFirstLineVisible()
+        {
+            // Check whether the first line is visible.
+            return _wpfTextView.ViewportTop == 0;
+        }
+
+        private bool GetIsLastLineVisible()
+        {
+            // Check whether the last line is visible.
+            try
+            {
+                var textViewLines = _wpfTextView.TextViewLines;
+                if (textViewLines != null && textViewLines.IsValid)
+                {
+                    var lastVisibleTextViewLine = textViewLines.LastVisibleLine;
+                    var lastVisibleLine = lastVisibleTextViewLine.Start.GetContainingLine();
+                    var lastVisibleLineNumber = lastVisibleLine.LineNumber;
+                    var snapshot = lastVisibleLine.Snapshot;
+                    if (lastVisibleLineNumber + 1 == snapshot.LineCount)
+                    {
+                        // Make sure the whole line is visible.
+                        var endOfLastVisibleLine = lastVisibleTextViewLine.EndIncludingLineBreak;
+                        if (endOfLastVisibleLine.Position == snapshot.Length)
+                        {
+                            return true;
+                        }
+                    }
+                }
+            }
+            catch (InvalidOperationException ex)
+            {
+                VimTrace.TraceError(ex);
+            }
+            return false;
+        }
     }
 }
