557e9eab004c4ccff277d1bac2bd8116db0025d7
nosami <jasonimison@gmail.com>
2019-11-07 09:53:56 +0000

    Install Mono 6.4.0.208 stable

--- Scripts/build.sh
+++ Scripts/build.sh
@@ -1,2 +1,5 @@
+wget https://download.visualstudio.microsoft.com/download/pr/5b7dcb51-3035-46f7-a8cb-efe3a1da351c/dcba976cd3257636b6b2828575d29d3c/monoframework-mdk-6.4.0.208.macos10.xamarin.universal.pkg
+sudo installer -pkg monoframework-mdk-6.4.0.208.macos10.xamarin.universal.pkg -target /
+
 msbuild /p:Configuration=ReleaseMac /p:Platform="Any CPU" /t:Restore
 msbuild /p:Configuration=ReleaseMac /p:Platform="Any CPU"
