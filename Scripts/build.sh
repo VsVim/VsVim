b63e9a2ef843d9e82c4ae926cad0bf0199b3f274
nosami <jasonimison@gmail.com>
2019-11-07 22:35:30 +0000

    Install VSMac 8.4 public preview

--- Scripts/build.sh
+++ Scripts/build.sh
@@ -1,5 +1,10 @@
 wget https://download.visualstudio.microsoft.com/download/pr/5b7dcb51-3035-46f7-a8cb-efe3a1da351c/dcba976cd3257636b6b2828575d29d3c/monoframework-mdk-6.4.0.208.macos10.xamarin.universal.pkg
 sudo installer -pkg monoframework-mdk-6.4.0.208.macos10.xamarin.universal.pkg -target /
 
-msbuild /p:Configuration=ReleaseMac /p:Platform="Any CPU" /t:Restore
-msbuild /p:Configuration=ReleaseMac /p:Platform="Any CPU"
+wget https://download.visualstudio.microsoft.com/download/pr/82f53c42-6dc7-481b-82e1-c899bb15a753/df08f05921d42cc6b3b02e9cb082841f/visualstudioformac-8.4.0.2350.dmg
+
+sudo hdiutil attach visualstudioformac-8.4.0.2350.dmg
+
+ditto -rsrc "/Volumes/Visual Studio/" /Applications/
+
+msbuild /p:Configuration=ReleaseMac /p:Platform="Any CPU" /t:Restore /t:Build
