632f84295ea7bfc39b7225d70585c447c788f047
nosami <jasonimison@gmail.com>
2019-11-08 17:58:38 +0000

    Publish artifact

--- Src/VimMac/Properties/AddinInfo.cs
+++ Src/VimMac/Properties/AddinInfo.cs
@@ -13,5 +13,3 @@ using Mono.Addins.Description;
 [assembly: AddinUrl("https://github.com/VsVim/VsVim")]
 [assembly: AddinDescription("VIM emulation layer for Visual Studio")]
 [assembly: AddinAuthor("Jared Parsons")]
-[assembly: AddinDependency("::MonoDevelop.Core", "8.3")]
-[assembly: AddinDependency("::MonoDevelop.Ide", "8.3")]
