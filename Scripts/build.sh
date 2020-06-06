e120dbf62ec376dc47334ca4cd4f7978c736a775
nosami <jasonimison@gmail.com>
2020-06-06 07:59:41 +0100

    Update build dependencies to Mono 6.10.0.104 and VSMac 8.6.2.6
    Fixes https://github.com/VsVim/VsVim/issues/2832

--- Scripts/build.sh
+++ Scripts/build.sh
@@ -1,12 +1,12 @@
 echo "Downloading Mono"
-wget --quiet https://download.visualstudio.microsoft.com/download/pr/5b7dcb51-3035-46f7-a8cb-efe3a1da351c/dcba976cd3257636b6b2828575d29d3c/monoframework-mdk-6.4.0.208.macos10.xamarin.universal.pkg
+wget --quiet https://download.visualstudio.microsoft.com/download/pr/15b48a0a-f363-47d8-9687-b7b59dbafd10/7fb3f0ff97647cb6c24bbdc36f2aad9c/monoframework-mdk-6.10.0.104.macos10.xamarin.universal.pkg
 
-sudo installer -pkg monoframework-mdk-6.4.0.208.macos10.xamarin.universal.pkg -target /
+sudo installer -pkg monoframework-mdk-6.10.0.104.macos10.xamarin.universal.pkg -target /
 
 echo "Downloading VSMac"
-wget --quiet https://download.visualstudio.microsoft.com/download/pr/82f53c42-6dc7-481b-82e1-c899bb15a753/df08f05921d42cc6b3b02e9cb082841f/visualstudioformac-8.4.0.2350.dmg
+wget --quiet https://download.visualstudio.microsoft.com/download/pr/68ffa29a-6a5b-41f7-af7b-506ddcf4bbfc/6f601f3b5ff4d19ecd2c258517acc562/visualstudioformac-8.6.2.6.dmg
 
-sudo hdiutil attach visualstudioformac-8.4.0.2350.dmg
+sudo hdiutil attach visualstudioformac-8.6.2.6.dmg
 
 echo "Removing pre-installed VSMac"
 sudo rm -rf "/Applications/Visual Studio.app"
@@ -19,7 +19,7 @@ rm -rf ~/Library/Preferences/Xamarin/
 rm -rf ~/Library/Developer/Xamarin
 rm -rf ~/Library/Application\ Support/VisualStudio
 
-echo "Installing VSMac 8.4"
+echo "Installing VSMac 8.6"
 ditto -rsrc "/Volumes/Visual Studio/" /Applications/
 
 msbuild /p:Configuration=ReleaseMac /p:Platform="Any CPU" /t:Restore /t:Build
