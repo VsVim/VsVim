f6bdefe7a91bff3ff5582006a2cbeecb37083b91
Jared Parsons <jaredpparsons@gmail.com>
2021-06-29 18:25:23 -0700

    Remove last of VsSpecific content
    This completely removes the `ISharedService` interface as well as all
    of the supporting VsSpecific libraries and shared code files.

--- Src/VsSpecific/VsSpecific/AssemblyInfo.cs
+++
@@ -1,6 +0,0 @@
-﻿using System.Reflection;
-using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
-
-[assembly: InternalsVisibleTo("VsVim.UnitTest")]
-[assembly: InternalsVisibleTo("DynamicProxyGenAssembly2")] // Moq
