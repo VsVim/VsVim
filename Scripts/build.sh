a33b5636283ad46e30e79cfe2e12ab9eaee0e440
nosami <jasonimison@gmail.com>
2022-07-22 13:37:34 +0100

    Use the newly downloaded dotnet to build

--- Scripts/build.sh
+++ Scripts/build.sh
@@ -7,16 +7,16 @@ echo "Installing VSMac 17.3 Preview"
 ditto -rsrc "/Volumes/Visual Studio (Preview)/" /Applications/
 mv "/Applications/Visual Studio (Preview).app" "/Applications/Visual Studio.app"
 
-echo "Installing dotnet 6.0.3xx"
+echo "installing dotnet 6.0.3xx"
 wget https://dot.net/v1/dotnet-install.sh
 bash dotnet-install.sh --channel 6.0.3xx
-dotnet workload install macos
+sudo ~/.dotnet/dotnet workload install macos
 
 echo "Building the extension"
-dotnet msbuild /p:Configuration=ReleaseMac /p:Platform="Any CPU" /t:Restore
+~/.dotnet/dotnet msbuild /p:Configuration=ReleaseMac /p:Platform="Any CPU" /t:Restore
 cd Src/VimMac
-dotnet msbuild /p:Configuration=ReleaseMac /p:Platform="Any CPU" /t:Build
+~/.dotnet/dotnet msbuild /p:Configuration=ReleaseMac /p:Platform="Any CPU" /t:Build
 
 echo "Creating and installing Extension"
 # Generate mpack extension artifact
-dotnet msbuild VimMac.csproj /t:InstallAddin
+~/.dotnet/dotnet msbuild VimMac.csproj /t:InstallAddin
