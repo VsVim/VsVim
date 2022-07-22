1546b13edbcd2f9b95274a1c06797d3205816c67
nosami <jasonimison@gmail.com>
2022-07-22 12:32:24 +0100

    Update install location

--- Scripts/build.sh
+++ Scripts/build.sh
@@ -1,18 +1,11 @@
 echo "Downloading VSMac"
 wget --quiet https://download.visualstudio.microsoft.com/download/pr/bc5903bb-4aab-497e-a097-1eb1a5e02645/3ff8f8cb722e81e2538bece0b82fa44f/visualstudioformac-preview-17.3.0.2012-pre.4-x64.dmg
 
-sudo hdiutil attach visualstudioformac-preview-17.3.0.2012-pre.4-x64.dmg
-
-rm -rf ~/Library/Preferences/VisualStudio
-rm -rf ~/Library/Preferences/Visual\ Studio
-rm -rf ~/Library/Logs/VisualStudio
-rm -rf ~/Library/VisualStudio
-rm -rf ~/Library/Preferences/Xamarin/
-rm -rf ~/Library/Developer/Xamarin
-rm -rf ~/Library/Application\ Support/VisualStudio
+hdiutil attach visualstudioformac-preview-17.3.0.2012-pre.4-x64.dmg
 
 echo "Installing VSMac 17.3 Preview"
 ditto -rsrc "/Volumes/Visual Studio (Preview)/" /Applications/
+mv "/Applications/Visual Studio (Preview).app" "/Applications/Visual Studio.app"
 
 echo "Installing dotnet 6.0.3xx"
 wget https://dot.net/v1/dotnet-install.sh
