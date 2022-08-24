ebcdcb1da78facaead32b4aa793cb4feb763a0fd
nosami <jasonimison@gmail.com>
2022-08-24 05:12:18 +0100

    Update volume path

--- Scripts/build.sh
+++ Scripts/build.sh
@@ -4,9 +4,9 @@ wget --quiet $url
 
 hdiutil attach `basename $url`
 
-echo "Installing VSMac 17.3 Preview"
-ditto -rsrc "/Volumes/Visual Studio (Preview)/" /Applications/
-mv "/Applications/Visual Studio (Preview).app" "/Applications/Visual Studio.app"
+echo "Installing VSMac 17.3"
+ditto -rsrc "/Volumes/Visual Studio/" /Applications/
+ls -la /Applications/
 
 echo "installing dotnet 6.0.3xx"
 wget https://dot.net/v1/dotnet-install.sh
@@ -16,7 +16,7 @@ sudo ~/.dotnet/dotnet workload install macos
 echo "Building the extension"
 ~/.dotnet/dotnet msbuild /p:Configuration=ReleaseMac /p:Platform="Any CPU" /t:Restore
 cd Src/VimMac
-~/.dotnet/dotnet msbuild /p:Configuration=ReleaseMac /p:Platform="Any CPU" /t:Build /v:diag
+~/.dotnet/dotnet msbuild /p:Configuration=ReleaseMac /p:Platform="Any CPU" /t:Build
 
 echo "Creating and installing Extension"
 # Generate mpack extension artifact
